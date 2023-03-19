90e2f4e52938f70d7ef0b669ef9bbe1650b44ceb
REDACTED-4 <REDACTED-4@student.uibk.ac.at>
2023-03-19 12:05:42 +0100

    basic entity implementation of Measurement

--- src/main/java/at/qe/skeleton/model/Measurement.java
+++ src/main/java/at/qe/skeleton/model/Measurement.java
@@ -1,4 +1,37 @@
 package at.qe.skeleton.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "MEASUREMENT")
 public class Measurement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ID;
+
+    @Column(name = "TIMESTAMP")
+    private LocalDateTime timestamp;
+
+    @Column(name = "DATA")
+    @OneToOne
+    @JoinColumn(name = "VALUES_ID")
+    private SensorValues sensorValues;
+
+    public Measurement() {
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public SensorValues getSensorValues() {
+        return sensorValues;
+    }
 }
