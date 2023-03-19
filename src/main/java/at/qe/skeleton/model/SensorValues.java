0f12af2bf58b5458227d5bcec49b6af264bb216c
Luana Bach Gadelha <luana.bach-gadelha@student.uibk.ac.at>
2023-03-19 11:55:11 +0100

    basic entity implementation of AccessPoint

--- src/main/java/at/qe/skeleton/model/SensorValues.java
+++ src/main/java/at/qe/skeleton/model/SensorValues.java
@@ -1,4 +1,11 @@
 package at.qe.skeleton.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "SENSOR_VALUES")
 public class SensorValues {
+
+
 }
