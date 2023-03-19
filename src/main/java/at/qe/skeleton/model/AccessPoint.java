84ae56705c60f005c35a0a9c4e07236c7c12937b
REDACTED-4 <REDACTED-4@student.uibk.ac.at>
2023-03-19 11:55:11 +0100

    basic entity implementation of AccessPoint

--- src/main/java/at/qe/skeleton/model/AccessPoint.java
+++ src/main/java/at/qe/skeleton/model/AccessPoint.java
@@ -1,4 +1,53 @@
 package at.qe.skeleton.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ACCESS_POINT")
 public class AccessPoint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ID;
+
+    @Column(name = "ROOM_NAME", nullable = false)
+    private String roomName;
+
+    @Column(name = "LAST_UPDATE")
+    private LocalDateTime lastUpdate;
+
+    @OneToOne
+    @JoinColumn(name = "VALUES_ID", nullable = false)
+    private SensorValues lowerBound;
+
+    @OneToOne
+    @JoinColumn(name = "VALUES_ID", nullable = false)
+    private SensorValues upperBound;
+
+    public AccessPoint() {
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public SensorValues getLowerBound() {
+        return lowerBound;
+    }
+
+    public SensorValues getUpperBound() {
+        return upperBound;
+    }
 }
