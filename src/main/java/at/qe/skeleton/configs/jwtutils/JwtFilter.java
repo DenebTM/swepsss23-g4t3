b018a77c824fbacdf5028034115a9fcd8b9773d8
REDACTED-3 <REDACTED-3@student.uibk.ac.at>
2023-03-22 13:02:42 +0100

    added class LoginController, changed from javax.servlet do jakarta.servlet

--- src/main/java/at/qe/skeleton/configs/jwtutils/JwtFilter.java
+++ src/main/java/at/qe/skeleton/configs/jwtutils/JwtFilter.java
@@ -1,11 +1,10 @@
 package at.qe.skeleton.configs.jwtutils;
 
 import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
