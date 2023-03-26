992a15f38410e1b3eaeef2c20b8b36d113770030
REDACTED-4 <REDACTED-4@student.uibk.ac.at>
2023-03-26 17:06:17 +0200

    added repositories for main entities

---
+++ src/main/java/at/qe/skeleton/repositories/SensorStationRepository.java
@@ -0,0 +1,8 @@
+package at.qe.skeleton.repositories;
+
+import at.qe.skeleton.model.SensorStation;
+
+public interface SensorStationRepository extends AbstractRepository<SensorStation, Long>{
+
+    SensorStation findFirstById(Long id);
+}
