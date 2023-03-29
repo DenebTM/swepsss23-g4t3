dc419cd768bdd13cbe063b312bfd3fef21fc92d7
Jamie Fox <james.fox@student.uibk.ac.at>
2023-03-29 16:59:43 +0200

    Add routing to frontend

---
+++ frontend/src/components/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/**
+ * Login page
+ */
+export const Login: React.FC = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>TODO: Log in</p>
+    </div>
+  )
+}
