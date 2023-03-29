5bc76c9f937a372c68490140cb339a65036d060c
DenebTM <deneb@screee.ee>
2023-03-29 20:49:07 +0200

    Add LED "driver"

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include <ble.h>
+#include <led.h>
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
   ble_setup();
+
+  led::setup();
+  led::set_color(led::WHITE);
 }
 
 void loop() {
