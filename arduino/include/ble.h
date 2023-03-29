44cde2cbcce89fb80fa8d57ab8ed2bc7d164f014
DenebTM <deneb@screee.ee>
2023-03-29 21:51:19 +0200

    Change device name

--- arduino/include/ble.h
+++ arduino/include/ble.h
@@ -5,7 +5,7 @@
 #include <Arduino.h>
 #include <ArduinoBLE.h>
 
-#define BLE_DEVICE_NAME "SensorStation G4T3"
+#define BLE_DEVICE_NAME "PH SensorStation"
 #define BLE_DEVICE_MANUFACTURER "UIBK SE G4T3"
 #define BLE_DEVICE_APPEARANCE 0x3621
 
