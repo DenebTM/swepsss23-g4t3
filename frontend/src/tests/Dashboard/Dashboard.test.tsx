da02d38e194236d9a464525eb07f14ea0c6ebc48
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-03-30 18:41:02 +0200

    Add constant nagivate mock function

--- frontend/src/tests/Dashboard/Dashboard.test.tsx
+++ frontend/src/tests/Dashboard/Dashboard.test.tsx
@@ -1,12 +1,6 @@
 import { render } from '@testing-library/react'
-import { test, vi } from 'vitest'
+import { test } from 'vitest'
 import { Dashboard } from '~/components/dashboard/Dashboard'
-
-vi.mock('react-router-dom', () => ({
-  useNavigate: () => {
-    /* do nothing */
-  },
-}))
 
 test('render dashboard without crashing', async () => {
   render(<Dashboard />)
