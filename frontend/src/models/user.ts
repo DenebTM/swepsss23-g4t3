bf767a4900743031e395bed5e8c436dd2be0f26b
Jamie Fox <james.fox@student.uibk.ac.at>
2023-03-30 17:53:10 +0200

    Add frontend models

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -1,5 +1,21 @@
+import { Timestamp } from './timestamp'
+
+/**
+ * A single user
+ */
 export interface User {
+  created: Timestamp
   firstName: string
   lastName: string
   username: string
+  role: UserRole
+}
+
+/**
+ * Possible roles that a {@link User} can have.
+ */
+export enum UserRole {
+  USER = 'USER',
+  GARDENER = 'GARDENER',
+  ADMIN = 'ADMIN',
 }
