10098fb32b16be323f51b0d5e41017f6d44c6615
Jamie Fox <james.fox@student.uibk.ac.at>
2023-03-30 18:08:52 +0200

    Migrate build target to esnext

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir: "./dist",
+    outDir: './dist',
+    target: 'esnext', // Should match the target in tsconfig.json
   },
   server: {
     port: 3000,
@@ -12,24 +13,24 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./config/jest/jest-setup.ts",
-    reporters: ["vitest-sonar-reporter", "default"],
-    outputFile: "sonar-report.xml",
+    environment: 'jsdom',
+    setupFiles: './config/jest/jest-setup.ts',
+    reporters: ['vitest-sonar-reporter', 'default'],
+    outputFile: 'sonar-report.xml',
     coverage: {
-      reporter: ["text", "text-summary", "html", "lcovonly"],
+      reporter: ['text', 'text-summary', 'html', 'lcovonly'],
       exclude: [
-        "node_modules/",
-        "config/jest/jest-setup.ts",
-        "src/tests",
-        "src/api/testData.ts",
+        'node_modules/',
+        'config/jest/jest-setup.ts',
+        'src/tests',
+        'src/api/testData.ts',
       ],
     },
   },
   resolve: {
     alias: {
-      "@component-lib": path.resolve(__dirname, "./src/components/lib"),
-      "~": path.resolve(__dirname, "./src"),
+      '@component-lib': path.resolve(__dirname, './src/components/lib'),
+      '~': path.resolve(__dirname, './src'),
     },
   },
-});
+})
