0ed1ba72065407c47c7b971f4bc22ee0b2ceb166
Jamie Fox <james.fox@student.uibk.ac.at>
2023-03-30 18:41:02 +0200

    Add constant nagivate mock function

--- frontend/config/jest/jest-setup.ts
+++ frontend/config/jest/jest-setup.ts
@@ -5,6 +5,23 @@
 import '@testing-library/jest-dom'
 import { vi } from 'vitest'
 
+/** Mock for  react-router-dom navigation function as this can not be run during tests. */
+export const NAVIGATE_MOCK = vi.fn()
+
+/**
+ * Functions like `useNavigate` and `useParams` are  incompatible with testing individual components,
+ * because they expects the React routing context to be present.
+ * This mock prevents these calls from throwing an error.
+ *
+ * It also helps with testing - for example, the following code snippet can be
+ * used to to assert that navigation to '/test/path' was attempted:
+ *
+ * `expect(NAVIGATE_MOCK).toHaveBeenCalledWith('/test/path')`
+ */
+vi.mock('react-router-dom', () => ({
+  useNavigate: () => NAVIGATE_MOCK,
+}))
+
 afterEach(() => {
   vi.clearAllMocks()
 })
