fd281487a5501e4639df1da64a9635d8b89f03f6
csaz3812 <a.brandstaetter@student.uibk.ac.at>
2023-03-31 11:51:14 +0200

    bugfix

--- src/main/java/at/qe/skeleton/services/SensorStationService.java
+++ src/main/java/at/qe/skeleton/services/SensorStationService.java
@@ -4,15 +4,17 @@ import at.qe.skeleton.model.SensorStation;
 import at.qe.skeleton.repositories.SensorStationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+@Service
 public class SensorStationService {
 
     @Autowired
     SensorStationRepository ssRepository;
 
-    @PreAuthorize("hasAnyAuthority('ADMIN', 'GARDENER)")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'GARDENER')")
     public Collection<SensorStation> getAllSS() {
         return ssRepository.findAll();
     }
