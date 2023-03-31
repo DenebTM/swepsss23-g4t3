d963d6a87fa2e418f420a9b32d7e03640e66b46a
REDACTED-3 <REDACTED-3@student.uibk.ac.at>
2023-03-31 11:45:17 +0200

    added getAllSS() in serviceclass and ROUTE to GET all sensor-stations

--- src/main/java/at/qe/skeleton/controllers/api/SensorStationRestController.java
+++ src/main/java/at/qe/skeleton/controllers/api/SensorStationRestController.java
@@ -1,11 +1,29 @@
 package at.qe.skeleton.controllers.api;
 
+import at.qe.skeleton.services.SensorStationService;
+import at.qe.skeleton.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SensorStationRestController implements BaseRestController{
 
+    @Autowired
+    private SensorStationService ssService;
 
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Route to GET all sensor-stations, available for all users
+     * @return List of all sensor-stations
+     */
+    @GetMapping(value ="/sensor-stations")
+    public ResponseEntity<Object> getAllSensorStations() {
+        return ResponseEntity.ok(ssService.getAllSS());
+    }
 
 
 
