bda3472a898a50b3711f0e7fb479508bba7e7aa6
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-03-31 21:06:23 +0200

    Add routes to get and delete a single sensor station by id

--- frontend/src/api/endpoints/sensorStations.ts
+++ frontend/src/api/endpoints/sensorStations.ts
@@ -1,9 +1,9 @@
 import { Server } from 'miragejs'
-import { _get } from '~/api/intercepts'
+import { _delete, _get } from '~/api/intercepts'
 import { SensorStation, SensorStationUuid } from '~/models/sensorStation'
 
 import { AppSchema, EndpointReg } from '../mirageTypes'
-import { success } from './helpers'
+import { notFound, success } from './helpers'
 
 /** URI for sensor stations */
 export const SENSOR_STATIONS = '/sensor-stations'
@@ -21,10 +21,20 @@ export const getSensorStations = async (): Promise<SensorStation[]> => {
  * Get a single access point by ID
  * @returns The fetched access point
  */
-export const getAccessPoint = async (
+export const getSensorStation = async (
   sensorStationUuid: SensorStationUuid
 ): Promise<SensorStation> => {
   return _get(`${SENSOR_STATIONS}/${sensorStationUuid}`)
+}
+
+/**
+ * DEL /api/sensor-stations/${uuid}
+ * Delete a single access point by ID
+ */
+export const deleteSensorStation = async (
+  sensorStationUuid: SensorStationUuid
+): Promise<SensorStation> => {
+  return _delete(`${SENSOR_STATIONS}/${sensorStationUuid}`)
 }
 
 /** Mocked sensor station functions */
@@ -33,5 +43,26 @@ export const mockedSensorStationReqs: EndpointReg = (server: Server) => {
   server.get(SENSOR_STATIONS, (schema: AppSchema, request) => {
     const sensorStations = schema.all('sensorStation')
     return success(sensorStations.models)
+  })
+
+  /** Mock {@link getSensorStation} */
+  server.get(`${SENSOR_STATIONS}/:uuid`, (schema: AppSchema, request) => {
+    const uuid: SensorStationUuid = Number(request.params.uuid)
+    const sensorStation = schema.findBy('sensorStation', { uuid: uuid })
+
+    return sensorStation ? sensorStation.attrs : notFound('sensor station')
+  })
+
+  /** Mock {@link deleteSensorStation} */
+  server.delete(`${SENSOR_STATIONS}/:uuid`, (schema: AppSchema, request) => {
+    const uuid: SensorStationUuid = Number(request.params.uuid)
+    const sensorStation = schema.findBy('sensorStation', { uuid: uuid })
+
+    if (sensorStation) {
+      sensorStation.destroy()
+      return success()
+    } else {
+      return notFound('sensor station')
+    }
   })
 }
