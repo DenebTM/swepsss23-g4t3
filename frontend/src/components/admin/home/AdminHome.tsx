58c52defb287668f7d6ef78de5c49fff8870a3c2
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-03-31 17:04:29 +0200

    Add PageWrapper to all pages

--- frontend/src/components/admin/home/AdminHome.tsx
+++ frontend/src/components/admin/home/AdminHome.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import { PageWrapper } from '~/components/page/PageWrapper'
+import { UserRole } from '~/models/user'
+
 /**
  * Admin home page. Links to all other admin pages.
  */
 export const AdminHome: React.FC = () => {
-  return <div>Admin home</div>
+  return <PageWrapper requiredRole={UserRole.ADMIN}>Admin home</PageWrapper>
 }
