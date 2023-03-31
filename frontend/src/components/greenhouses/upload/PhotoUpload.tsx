04cc223b1e8f049f2f9e1a3b21d4ee7049325e8b
Jamie Fox <james.fox@student.uibk.ac.at>
2023-03-31 17:04:29 +0200

    Add PageWrapper to all pages

--- frontend/src/components/greenhouses/upload/PhotoUpload.tsx
+++ frontend/src/components/greenhouses/upload/PhotoUpload.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import { PageWrapper } from '~/components/page/PageWrapper'
+
 /**
  * Page to upload a photo for a single sensor station
  */
 export const PhotoUpload: React.FC = () => {
-  return <div>Photo upload page</div>
+  return <PageWrapper>Photo upload page</PageWrapper>
 }
