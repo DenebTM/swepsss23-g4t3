2fadac218391ecdf21de8a6734a871f9c13e25a2
Jamie Fox <james.fox@student.uibk.ac.at>
2023-03-31 20:56:52 +0200

    Add routes to get and delete a single user

--- frontend/src/api/endpoints/user.ts
+++ frontend/src/api/endpoints/user.ts
@@ -1,9 +1,9 @@
 import { Server } from 'miragejs'
-import { _get } from '~/api/intercepts'
-import { User } from '~/models/user'
+import { _delete, _get } from '~/api/intercepts'
+import { User, Username } from '~/models/user'
 
 import { AppSchema, EndpointReg } from '../mirageTypes'
-import { success } from './helpers'
+import { notFound, success } from './helpers'
 
 /** URI for users routes */
 export const USERS = '/users'
@@ -16,11 +16,51 @@ export const getUsers = async (): Promise<User[]> => {
   return _get(USERS)
 }
 
+/**
+ * Get a single user by username
+ * GET /api/users/${username}
+ * @returns All users in the database
+ */
+export const getUser = async (username: Username): Promise<User> => {
+  return _get(`${USERS}/${username}`)
+}
+
+/**
+ * Delete a single user by username
+ * DEL /api/users/${username}
+ */
+export const deleteUser = async (username: Username): Promise<void> => {
+  return _delete(`${USERS}/${username}`)
+}
+
 /** Mocked users functions */
 export const mockedUserReqs: EndpointReg = (server: Server) => {
   /** Mock {@link getUsers} */
   server.get(USERS, (schema: AppSchema, request) => {
     const users = schema.all('user')
     return success(users.models)
+  })
+
+  /** Mock {@link getUser} */
+  server.get(`${USERS}/:username`, (schema: AppSchema, request) => {
+    const username: Username = request.params.username
+    const user = schema.findBy('user', { username: username })
+    if (user) {
+      return success(user.attrs)
+    } else {
+      return notFound('user')
+    }
+  })
+
+  /** Mock {@link deleteUser} */
+  server.delete(`${USERS}/:username`, (schema: AppSchema, request) => {
+    const username: Username = request.params.username
+    const user = schema.findBy('user', { username: username })
+    if (user) {
+      user.destroy()
+      return success()
+    } else {
+      return notFound('user')
+    }
   })
 }
