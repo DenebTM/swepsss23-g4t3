82878f346616f7bb6daf02948143bad400547a2d
csaz3812 <a.brandstaetter@student.uibk.ac.at>
2023-03-31 12:19:43 +0200

    added getAllAP and loadAPById() in serviceclass and ROUTE to GET all and single access-points by ID and deleted some unused imports

---
+++ src/main/java/at/qe/skeleton/controllers/api/AccessPointRestController.java
@@ -0,0 +1,43 @@
+package at.qe.skeleton.controllers.api;
+
+import at.qe.skeleton.model.AccessPoint;
+import at.qe.skeleton.services.AccessPointService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccessPointRestController {
+
+    @Autowired
+    private AccessPointService apService;
+
+    /**
+     * Route to GET all access-points, available for all users
+     * @return List of all access-points
+     */
+    @GetMapping(value ="/access-points")
+    public ResponseEntity<Object> getAllAccessPoints() {
+        return ResponseEntity.ok(apService.getAllAP());
+    }
+
+    /**
+     * Route to GET a specific access-point by its ID
+     * @param id
+     * @return access-point
+     */
+    @GetMapping(value="/access-points/{id}")
+    public ResponseEntity<Object> getAPById(@PathVariable(value = "id") Integer id) {
+        AccessPoint ap = apService.loadAPById(new Long(id));
+
+        // Return a 404 error if the access-points is not found
+        if (ap == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Access-points with id: \"" + id + "\" not found.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(ap);
+    }
+}
