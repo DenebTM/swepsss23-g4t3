0c2caecbebfb96dac08eae7c1f9743735c91066b
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-03-31 18:26:31 +0200

    Remove logout button from dashboard

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,15 +1,9 @@
 import { cancelable } from 'cancelable-promise'
 import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 
-import Button from '@mui/material/Button'
-
-import { logout } from '~/api/endpoints/login'
 import { getUsers } from '~/api/endpoints/user'
-import { URL } from '~/common'
 import { PageWrapper } from '~/components/page/PageWrapper'
 import { Message, MessageType } from '~/contexts/types'
-import { deleteJwt } from '~/helpers/jwt'
 import { useAddSnackbarMessage } from '~/hooks/snackbar'
 import { User } from '~/models/user'
 
@@ -17,7 +11,6 @@ import { User } from '~/models/user'
  * Dashboard page
  */
 export const Dashboard: React.FC = () => {
-  const navigate = useNavigate()
   const addSnackbarMessage = useAddSnackbarMessage()
   const [users, setUsers] = useState<User[]>([])
   const [snackbarMessage, setSnackbarMessage] = useState<Message | null>(null)
@@ -53,17 +46,6 @@ export const Dashboard: React.FC = () => {
     }
   }, [snackbarMessage])
 
-  const handleLogout = (): Promise<void> =>
-    logout()
-      .then(() => {
-        // Delete JWT cookie
-        deleteJwt()
-        navigate(URL.login)
-      })
-      .catch((err: Error) => {
-        throw err // qqjf TOOD: error handling
-      })
-
   return (
     <PageWrapper>
       <h1>Dashboard</h1>
@@ -72,10 +54,6 @@ export const Dashboard: React.FC = () => {
           <li key={u.username}>{u.firstName + ' ' + u.lastName}</li>
         ))}
       </ul>
-
-      <Button variant="contained" onClick={handleLogout}>
-        Logout
-      </Button>
     </PageWrapper>
   )
 }
