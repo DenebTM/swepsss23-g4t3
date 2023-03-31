c3c491f3678ee127245aa2a971d92ad4a75adc2f
Jamie Fox <james.fox@student.uibk.ac.at>
2023-03-31 22:26:42 +0200

    Add simple mocked api demo to dashboard

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,42 +1,33 @@
 import { cancelable } from 'cancelable-promise'
 import React, { useEffect, useState } from 'react'
 
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+
+import { assignGardener } from '~/api/endpoints/sensorStations/gardeners'
+import { getSensorStations } from '~/api/endpoints/sensorStations/sensorStations'
 import { getUsers } from '~/api/endpoints/user'
 import { PageWrapper } from '~/components/page/PageWrapper'
 import { Message, MessageType } from '~/contexts/SnackbarContext/types'
 import { useAddSnackbarMessage } from '~/hooks/snackbar'
-import { User } from '~/models/user'
+import { SensorStation } from '~/models/sensorStation'
+import { UserRole } from '~/models/user'
 
 /**
  * Dashboard page
  */
 export const Dashboard: React.FC = () => {
   const addSnackbarMessage = useAddSnackbarMessage()
-  const [users, setUsers] = useState<User[]>([])
+  const [sensorStations, setSensorStations] = useState<SensorStation[]>([])
   const [snackbarMessage, setSnackbarMessage] = useState<Message | null>(null)
 
   /** Load users from the API on component mount and set the value of {@link snackbarMessage} */
   useEffect(() => {
-    const usersPromise = cancelable(getUsers())
-    usersPromise
-      .then((data) => {
-        setUsers(data)
-        setSnackbarMessage({
-          header: '',
-          body: 'Loaded users!',
-          type: MessageType.CONFIRM,
-        })
-      })
-      .catch((err: Error) =>
-        setSnackbarMessage({
-          header: 'Could not load users',
-          body: err.message,
-          type: MessageType.ERROR,
-        })
-      )
+    const ssPromise = cancelable(getSensorStations())
+    loadSensorStations(ssPromise)
 
     // Cancel the promise callbacks on component unmount
-    return usersPromise.cancel
+    return ssPromise.cancel
   }, [])
 
   /** Create a new snackbar if {@link snackbarMessage} has been updated */
@@ -46,14 +37,69 @@ export const Dashboard: React.FC = () => {
     }
   }, [snackbarMessage])
 
+  /** Load sensor stations from the backend and set in state */
+  const loadSensorStations = (promise: Promise<SensorStation[]>) =>
+    promise
+      .then((data) => {
+        setSensorStations(data)
+      })
+      .catch((err: Error) =>
+        setSnackbarMessage({
+          header: 'Could not load sensor stations',
+          body: err.message,
+          type: MessageType.ERROR,
+        })
+      )
+
+  /** Demo function to assign a user to the first sensor station found */
+  const assignGardenerToSs = () => {
+    // qqjf obviously unsafe if sensorStations is empty but this is just a demo
+    const sensorStation = sensorStations[0]
+
+    getUsers().then((users) => {
+      const unassignedGardeners = users.filter(
+        (u) =>
+          !sensorStation.gardeners.includes(u.username) &&
+          u.role === UserRole.GARDENER
+      )
+      if (unassignedGardeners.length >= 0) {
+        assignGardener(
+          sensorStations[0].uuid,
+          unassignedGardeners[0].username
+        ).then(() => {
+          loadSensorStations(getSensorStations())
+        })
+      } else {
+        setSnackbarMessage({
+          header: '',
+          body: 'No gardeners to assign - try refreshing the page',
+          type: MessageType.ERROR,
+        })
+      }
+    })
+  }
+
   return (
     <PageWrapper>
       <h1>Dashboard</h1>
+      <h4>Sensor stations before action:</h4>
       <ul>
-        {users.map((u: User) => (
-          <li key={u.username}>{u.firstName + ' ' + u.lastName}</li>
+        {sensorStations.map((s: SensorStation) => (
+          <li key={s.uuid}>
+            {'Sensor station ' +
+              s.uuid +
+              ' has gardeners "' +
+              s.gardeners +
+              '" and AP "' +
+              s.accessPoint +
+              '"'}
+          </li>
         ))}
       </ul>
+      <Divider />
+      <Button variant="contained" onClick={assignGardenerToSs}>
+        Assign a user to sensor station 0
+      </Button>
     </PageWrapper>
   )
 }
