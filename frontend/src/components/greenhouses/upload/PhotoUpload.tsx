06db9d4fefe2deba2d5fd3aff4dc706e87492453
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-03-31 15:29:57 +0200

    Add dummy contents for upload page

--- frontend/src/components/greenhouses/upload/PhotoUpload.tsx
+++ frontend/src/components/greenhouses/upload/PhotoUpload.tsx
@@ -4,5 +4,5 @@ import React from 'react'
  * Page to upload a photo for a single sensor station
  */
 export const PhotoUpload: React.FC = () => {
-  return <></>
+  return <div>Photo upload page</div>
 }
