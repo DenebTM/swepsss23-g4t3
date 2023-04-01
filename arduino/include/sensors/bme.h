53594278d64a920dd2a615b0cdda365c8205de42
DenebTM <deneb@screee.ee>
2023-04-01 21:49:21 +0200

    Use BSEC2 library to obtain IAQ

--- arduino/include/sensors/bme.h
+++ arduino/include/sensors/bme.h
@@ -3,16 +3,17 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <bme68xLibrary.h>
+#include <bsec2.h>
 
 namespace sensors::bme {
   static Bme68x sensor;
+  static Bsec2 bsec;
 
   // returns true on success, false if the sensor could not be found
   bool setup();
 
   // returns true on success, false if an error occurred or the sensor could not be found
-  bool read();
+  bool update();
 }
 
 #endif
--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -5,11 +5,6 @@
 #include <led.h>
 #include <sensors/bme.h>
 
-bool read_sensor = false;
-void read_sensor_isr() {
-  read_sensor = true;
-}
-
 void setup() {
   Serial.begin(9600);
   // wait for the Serial port to be initialized so that error messages during setup don't get lost
@@ -21,18 +16,12 @@ void setup() {
   led::set_color(led::RED);
 
   sensors::bme::setup();
-
-  buttons::setup(1, read_sensor_isr);
-  Serial.println("Press button 1 (middle) to read and print BME688 sensor data");
 }
 
 void loop() {
   ble::update();
 
-  if (read_sensor) {
-    sensors::bme::read();
-    read_sensor = false;
-  }
+  sensors::bme::update();
 
   delay(10);
 }
