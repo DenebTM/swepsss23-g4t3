e257025c20bcf7704eb515a1893039fafebf9f4d
csaz3812 <a.brandstaetter@student.uibk.ac.at>
2023-04-01 09:00:48 +0200

    changed checking for userRole, added documentation for all controllers and services until now

--- src/main/java/at/qe/skeleton/services/SensorStationService.java
+++ src/main/java/at/qe/skeleton/services/SensorStationService.java
@@ -13,10 +13,20 @@ public class SensorStationService {
     @Autowired
     SensorStationRepository ssRepository;
 
+    /**
+     * Route to GET all sensor-stations
+     * @return List of all sensor-stations
+     */
     public Collection<SensorStation> getAllSS() {
         return ssRepository.findAll();
     }
 
+    /**
+     * Loads a single sensor-station identified by its id.
+     *
+     * @param id the id of sensor-station to search for
+     * @return the sensor-station with the given id
+     */
     public SensorStation loadSSById(Long id) {
         return ssRepository.findFirstById(id);
     }
