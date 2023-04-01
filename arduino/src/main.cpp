18c9bd09f106a531815077067da938b1d1ee0cf5
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-04-01 20:04:42 +0200

    Read data from BME688

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 
 #include <ble.h>
+#include <buttons.h>
 #include <led.h>
+#include <sensors/bme.h>
+
+bool read_sensor = false;
+void read_sensor_isr() {
+  read_sensor = true;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -12,9 +19,20 @@ void setup() {
 
   led::setup();
   led::set_color(led::RED);
+
+  sensors::bme::setup();
+
+  buttons::setup(1, read_sensor_isr);
+  Serial.println("Press button 1 (middle) to read and print BME688 sensor data");
 }
 
 void loop() {
   ble::update();
+
+  if (read_sensor) {
+    sensors::bme::read();
+    read_sensor = false;
+  }
+
   delay(10);
 }
