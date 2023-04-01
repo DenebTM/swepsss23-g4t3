9a201432206be6445b708771f9ebabd6ee834e63
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-04-01 21:49:21 +0200

    Use BSEC2 library to obtain IAQ

--- arduino/src/sensors/bme.cpp
+++ arduino/src/sensors/bme.cpp
@@ -1,57 +1,64 @@
+// based heavily on https://github.com/boschsensortec/Bosch-BSEC2-Library/blob/master/examples/generic_examples/basic/basic.ino
+
 #include <sensors/bme.h>
 
-void bme_i2c_delay(uint32_t period_us, void *intf_ptr) {
-	delayMicroseconds(period_us);
+namespace sensors::bme {
+  void sensorCallback(const bme68xData data, const bsecOutputs outputs, Bsec2 bsec) {
+    if (!outputs.nOutputs) {
+      Serial.println("BME688 - Received callback but no data");
+    }
+
+    Serial.println("BME688 - Sensor data:");
+    for (uint8_t i = 0; i < outputs.nOutputs; i++) {
+        const bsecData output  = outputs.output[i];
+        switch (output.sensor_id) {
+            case BSEC_OUTPUT_IAQ:
+                Serial.println("  iaq: " + String(output.signal));
+                Serial.println("    (accuracy: " + String((int) output.accuracy) + ")");
+                break;
+            case BSEC_OUTPUT_RAW_TEMPERATURE:
+                Serial.println("  temperature: " + String(output.signal));
+                break;
+            case BSEC_OUTPUT_RAW_PRESSURE:
+                Serial.println("  air pressure: " + String(output.signal));
+                break;
+            case BSEC_OUTPUT_RAW_HUMIDITY:
+                Serial.println("  humidity: " + String(output.signal));
+                break;
+        }
+    } 
+  }
 }
 
 bool sensors::bme::setup() {
+  /* Desired subscription list of BSEC2 outputs */
+  bsecSensor sensorList[] = {
+    BSEC_OUTPUT_IAQ,
+    BSEC_OUTPUT_RAW_TEMPERATURE,
+    BSEC_OUTPUT_RAW_PRESSURE,
+    BSEC_OUTPUT_RAW_HUMIDITY,
+  };
+
   Wire.begin();
-  
-  // Try to initialize communication with the sensor
-  sensor.begin(0x77, Wire, bme_i2c_delay);
-  if (int8_t sensorStatus = sensor.checkStatus()) {
-    if (sensorStatus == BME68X_ERROR) {
-      Serial.println("BME688 error: " + sensor.statusString());
-      return false;
-    }
+  if (!bsec.begin(BME68X_I2C_ADDR_HIGH, Wire)) {
+    Serial.println("BME688 - Error initializing sensor: " + String(bsec.sensor.status));
+    return false;
   }
 
-  // setup sensor as in https://github.com/boschsensortec/Bosch-BME68x-Library/blob/master/examples/sequential_mode/sequential_mode.ino
-  // (whatever the example does here is probably reasonable for us to do)
-  // Initialize temperature, pressure and humidity sensors with default configuration
-  sensor.setTPH();
+  if (!bsec.updateSubscription(sensorList, ARRAY_LEN(sensorList), BSEC_SAMPLE_RATE_LP)) {
+    Serial.println("BME688 - Error updating sensor data subscription: " + String(bsec.status));
+  }
 
-  /// Set sensor pre-heating profile
-  // Heater temperature in °C
-  uint16_t tempProf[] = { 100, 200, 320 };
-  // Heating duration in milliseconds
-	uint16_t durProf[] = { 150, 150, 150 };
-  sensor.setHeaterProf(tempProf, durProf, 3);
+  bsec.attachCallback(sensorCallback);
 
-  sensor.setSeqSleep(BME68X_ODR_250_MS);
-
-  Serial.println("BME688 initialized successfully!");
   return true;
 }
 
-bool sensors::bme::read() {
-  bme68xData data;
+bool sensors::bme::update() {
+  if (!bsec.run()) {
+    Serial.println("BME688 - Sensor error: " + String(bsec.status));
+    return false;
+  }
 
-  sensor.setOpMode(BME68X_FORCED_MODE);
-	delayMicroseconds(sensor.getMeasDur());
-
-	if (sensor.fetchData()) {
-    while (sensor.getData(data) > 0); // make sure all data has been read
-    
-    Serial.println("\nBME688 sensor data\n==============================");
-    Serial.println("Temperature: " + String(data.temperature) + "°C");
-    Serial.println("Pressure: " + String(data.pressure / 100) + " hPa");
-    Serial.println("Humidity: " + String(data.humidity) + "%");
-    Serial.println("Gas resistance: " + String(data.gas_resistance / 1000) + " kOhm");
-    Serial.println("Sensor status: " + String(data.status, HEX));
-
-    return true;
-	}
-  
-  return false;
+  return true;
 }
