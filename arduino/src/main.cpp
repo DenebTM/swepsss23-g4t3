368ba1e197e1b18893bf7c3f2a9905d05f9fbc29
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-04-02 14:52:13 +0200

    Calibrate and read data from hygrometer

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -4,24 +4,27 @@
 #include <buttons.h>
 #include <led.h>
 #include <sensors/bme.h>
+#include <sensors/hygro.h>
 
 void setup() {
   Serial.begin(9600);
   // wait for the Serial port to be initialized so that error messages during setup don't get lost
   while (!Serial);
 
-  ble::setup();
+  sensors::bme::setup();
+  sensors::hygro::setup();
 
   led::setup();
   led::set_color(led::RED);
 
-  sensors::bme::setup();
+  ble::setup();
 }
 
 void loop() {
   ble::update();
 
   sensors::bme::update();
+  sensors::hygro::update();
 
   delay(10);
 }
