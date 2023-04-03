7e77e25a7551155eda4b250f070abafe6d2c80a8
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-03 20:41:13 +0200

    Add DashboardCard and grid components

---
+++ frontend/src/components/dashboard/DashboardCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+
+interface DashboardCardProps {
+  /** The card contents */
+  children: React.ReactNode
+}
+
+/**
+ * Reusable component for the main elements in the Dashboard screen
+ */
+export const DashboardCard: React.FC<DashboardCardProps> = (props) => {
+  return (
+    <Card>
+      <CardContent>{props.children}</CardContent>
+    </Card>
+  )
+}
