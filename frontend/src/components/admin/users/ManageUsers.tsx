a6feef66aceb8b4c380ca2cc60f88789945fea60
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-07 20:00:46 +0200

    Add breadcrumbs to admin pages

--- frontend/src/components/admin/users/ManageUsers.tsx
+++ frontend/src/components/admin/users/ManageUsers.tsx
@@ -4,13 +4,15 @@ import { PageHeader } from '~/components/page/PageHeader'
 import { PageWrapper } from '~/components/page/PageWrapper'
 import { UserRole } from '~/models/user'
 
+import { AdminBreadcrumbs } from '../AdminBreadcrumbs'
+
 /**
  * User managment page for admins
  */
 export const ManageUsers: React.FC = () => {
   return (
     <PageWrapper requiredRole={UserRole.ADMIN}>
-      <PageHeader />
+      <PageHeader left={<AdminBreadcrumbs currentPageName="Manage Users" />} />
       Manage users
     </PageWrapper>
   )
