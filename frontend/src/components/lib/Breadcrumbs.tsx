20ae8c6e6120f81e407129fa18159310b2032642
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-07 20:00:09 +0200

    Add reusable breadcrumbs component

---
+++ frontend/src/components/lib/Breadcrumbs.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
+import MuiBreadcrumbs from '@mui/material/Breadcrumbs'
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+
+import { theme } from '~/styles/theme'
+
+export interface BreadcrumbLink {
+  /** The URL of to navigate to on click */
+  href: string
+  /** The name to display for the breadcrumb link. Should be unique within the list of links in order to use it as a key. */
+  name: string
+}
+
+interface BreadcrumbsProps {
+  /** Ordered list of links higher in the hierarchy than the current page */
+  links: BreadcrumbLink[]
+  /** The current page name (will not be hypgerlinked) */
+  currentPageName: string
+}
+
+/**
+ * Custom breadcrumbs component to show the user where they are in the page hierarchy
+ */
+export const Breadcrumbs: React.FC<BreadcrumbsProps> = (props) => {
+  const navigate = useNavigate()
+
+  return (
+    <MuiBreadcrumbs
+      separator={<NavigateNextIcon fontSize="small" />}
+      aria-label="breadcrumbs"
+      color={theme.outline}
+    >
+      {props.links.map((link: BreadcrumbLink) => (
+        <Link
+          underline="hover"
+          key={link.name}
+          color="inherit"
+          onClick={() => navigate(link.href)}
+          sx={{ cursor: 'pointer' }}
+        >
+          {link.name}
+        </Link>
+      ))}
+      <Typography color={theme.onSurfaceVariant}>
+        {props.currentPageName}
+      </Typography>
+    </MuiBreadcrumbs>
+  )
+}
