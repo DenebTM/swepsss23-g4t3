73476396b4429d4cc31ae6c4f3e100b207423b7d
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-04-07 20:22:17 +0200

    Pass view and uuid in props to avoid visual glitches on load

--- frontend/src/components/greenhouses/greenhouseView/greenhouseHeader/GreenhouseSegmentedButtons.tsx
+++ frontend/src/components/greenhouses/greenhouseView/greenhouseHeader/GreenhouseSegmentedButtons.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined'
@@ -7,15 +7,9 @@ import StorageOutlinedIcon from '@mui/icons-material/StorageOutlined'
 import ButtonGroup from '@mui/material/ButtonGroup'
 import { styled } from '@mui/material/styles'
 
-import { SensorStationView, SS_UUID_PARAM, URL } from '~/common'
-import { MessageType } from '~/contexts/SnackbarContext/types'
-import { useAddSnackbarMessage } from '~/hooks/snackbar'
+import { SensorStationView, URL } from '~/common'
 import { SensorStationUuid } from '~/models/sensorStation'
 
-import {
-  getSsUuidFromParams,
-  getViewFromSearchParams,
-} from '../greenhouseHelpers'
 import { SegmentedButton } from './SegmentedButton'
 
 const borderRadius = '40px'
@@ -32,28 +26,19 @@ const StyledButtonGroup = styled(ButtonGroup)(({ theme }) => ({
   },
 }))
 
+interface GreenhouseSegmentedButtonsProps {
+  uuid: SensorStationUuid
+  view: SensorStationView
+}
+
 /**
  * Button group showing the currently selected view in the greenhouse pages.
  * Infers which button should be selected from the URL search params.
  */
-export const GreenhouseSegmentedButtons: React.FC = () => {
-  const params = useParams()
-  const addSnackbarMessage = useAddSnackbarMessage()
-
-  const [search] = useSearchParams()
-  const [view, setView] = useState<SensorStationView>()
-  const [uuid, setUuid] = useState<SensorStationUuid>()
+export const GreenhouseSegmentedButtons: React.FC<
+  GreenhouseSegmentedButtonsProps
+> = (props) => {
   const navigate = useNavigate()
-
-  /** Get sensor station UUID from URL params */
-  useEffect(() => {
-    setUuid(getSsUuidFromParams(params))
-  }, [params[SS_UUID_PARAM]])
-
-  /** Get page view from seach (query) params */
-  useEffect(() => {
-    setView(getViewFromSearchParams(search))
-  }, [search])
 
   /** Define buttons for all views */
   const views = [
@@ -76,16 +61,7 @@ export const GreenhouseSegmentedButtons: React.FC = () => {
 
   /** Handle navigate to targetView on button click */
   const handleNavigate = (targetView: SensorStationView): void => {
-    if (typeof uuid !== 'undefined') {
-      navigate(URL.greenhouseView(uuid, targetView))
-    } else {
-      // If the UUID can not be inferred from the route
-      addSnackbarMessage({
-        header: 'Greenhouse ID could not be inferred from the URL',
-        body: 'Return to the Dashboard or try again later',
-        type: MessageType.ERROR,
-      })
-    }
+    navigate(URL.greenhouseView(props.uuid, targetView))
   }
 
   return (
@@ -96,8 +72,8 @@ export const GreenhouseSegmentedButtons: React.FC = () => {
             key={btn.key}
             onClick={() => handleNavigate(btn.key)}
             icon={btn.icon}
-            selected={view === btn.key}
-            aria-label={'Navigate to greenhouse' + btn.key.toLowerCase()}
+            selected={props.view === btn.key}
+            aria-label={'Navigate to greenhouse ' + btn.key.toLowerCase()}
           >
             {btn.name}
           </SegmentedButton>
