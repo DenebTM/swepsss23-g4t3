9d47e3b99292578e805a91c985ab7c968cbb1c1c
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-04-07 14:28:18 +0200

    Move row fetching into DataGrid

--- frontend/src/components/lib/DataGrid.tsx
+++ frontend/src/components/lib/DataGrid.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { cancelable } from 'cancelable-promise'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 import Box from '@mui/material/Box'
 import {
@@ -8,9 +9,16 @@ import {
   DataGridProps as MuiDataGridProps,
 } from '@mui/x-data-grid'
 
-import { MessageType } from '~/contexts/SnackbarContext/types'
+import { Message, MessageType } from '~/contexts/SnackbarContext/types'
 import { useAddSnackbarMessage } from '~/hooks/snackbar'
 
+/**
+ * Handle changes to editable cells. Note that this function does not need to update rows as this is handled inside MUI's DataGrid component
+    @template R The type of each row object.
+    @param newRow — Row object with the new values.
+    @param oldRow — Row object with the old values.
+    @returns — The final values to update the row.
+ */
 export type RowUpdateFunction<R extends GridValidRowModel> = (
   newRow: R,
   oldRow: R
@@ -18,26 +26,74 @@ export type RowUpdateFunction<R extends GridValidRowModel> = (
 
 /** Extend and limit {@link MuiDataGridProps} to provide additional type hints and safety. */
 interface DataGridProps<R extends GridValidRowModel, V, F>
-  extends MuiDataGridProps<R> {
+  extends Omit<MuiDataGridProps<R>, 'rows'> {
+  // DataGrid props
+  /** The column definition for the table */
   columns: GridColDef<R, V, F>[]
-  loading: boolean
+  /**
+   * Handle changes to editable cells.
+   * Note that this function does *not* need to update `rows` as this is handled inside MUI's DataGrid component
+   */
   processRowUpdate: RowUpdateFunction<R>
-  rows: readonly R[]
+  /** Rows to display in the table. Pagination is handled internally. If undefined, then display a loading indicator. */
+  rows: readonly R[] | undefined
+
+  // Additional props
+  /** Function to fetch row data. Error handling and state updates are then handled internally using `setRows`. */
+  fetchRows: () => Promise<R[]>
+  /** Function to update row data in the state of the parent component. */
+  setRows: Dispatch<SetStateAction<R[] | undefined>>
 }
 
 /**
- * Wrapper component for MUI's DataGrid component.
- * Use to display filterable, editable table contents.
+ * Wrapper component for MUI's DataGrid component. Use to display filterable, editable table contents.
+ *
+ * Handles fetching rows from the backend, state updates in the parent component, and loading states internally
+ * and displays error snackbars if fetching or updating rows fails.
  */
 export const DataGrid = <R extends GridValidRowModel, V, F = V>(
   props: DataGridProps<R, V, F>
 ): React.ReactElement => {
   const addSnackbarMessage = useAddSnackbarMessage()
-  const { ...gridProps } = props
+  const { fetchRows, setRows, rows, ...gridProps } = props
 
+  const [snackbarMessage, setSnackbarMessage] = useState<Message | null>(null)
+
+  /** Load rows from the API on component mount */
+  useEffect(() => {
+    const rowsPromise = cancelable(props.fetchRows())
+    handleFetchRows(rowsPromise)
+
+    // Cancel the promise callbacks on component unmount
+    return rowsPromise.cancel
+  }, [])
+
+  /** Create a new snackbar if {@link snackbarMessage} has been updated */
+  useEffect(() => {
+    if (snackbarMessage !== null) {
+      addSnackbarMessage(snackbarMessage)
+    }
+  }, [addSnackbarMessage, snackbarMessage])
+
+  /** Load rows from the backend and set in the state of the parent component. */
+  const handleFetchRows = (promise: Promise<R[]>) =>
+    promise
+      .then((data) => {
+        props.setRows(data)
+      })
+      .catch((err: Error) => {
+        props.setRows([]) // Remove loading state
+        setSnackbarMessage({
+          header: 'Could not load table contents',
+          body: err.message,
+          type: MessageType.ERROR,
+        })
+      })
+
+  /** If updating a row fails, then display a snackbar informing the user. */
   const onProcessRowUpdateError = (err: Error) =>
     addSnackbarMessage({
-      header: 'Could not load users',
+      header: 'Could not update row',
       body: err.message,
       type: MessageType.ERROR,
     })
@@ -56,6 +112,8 @@ export const DataGrid = <R extends GridValidRowModel, V, F = V>(
         isRowSelectable={() => false}
         pageSizeOptions={[5, 10, 25]}
         onProcessRowUpdateError={onProcessRowUpdateError}
+        loading={typeof props.rows === 'undefined'}
+        rows={typeof props.rows === 'undefined' ? [] : props.rows}
         {...gridProps}
       />
     </Box>
