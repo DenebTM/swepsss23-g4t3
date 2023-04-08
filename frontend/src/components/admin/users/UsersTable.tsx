739870deb2ba3409ff3465bd6ff1c40f9d4ad21d
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-08 17:30:15 +0200

    Add delete confirmation dialog to admin users table

--- frontend/src/components/admin/users/UsersTable.tsx
+++ frontend/src/components/admin/users/UsersTable.tsx
@@ -1,53 +1,17 @@
 import React, { Dispatch, SetStateAction } from 'react'
 
-import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
+import {
+  GridColDef,
+  GridRenderCellParams,
+  GridValueGetterParams,
+} from '@mui/x-data-grid'
 
 import { DataGrid, RowUpdateFunction } from '@component-lib/DataGrid'
 import dayjs from 'dayjs'
 import { getUsers, updateUser } from '~/api/endpoints/user'
 import { User, UserRole } from '~/models/user'
 
-/** Columns for the user managmement table */
-const columns: GridColDef<User, any, User>[] = [
-  { field: 'username', headerName: 'Username', flex: 1 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    editable: true,
-    flex: 1,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    editable: true,
-    flex: 1,
-  },
-  {
-    field: 'role',
-    headerName: 'Role',
-    type: 'singleSelect',
-    valueOptions: [
-      { value: UserRole.ADMIN, label: 'ADMIN' },
-      { value: UserRole.GARDENER, label: 'GARDENER' },
-      { value: UserRole.USER, label: 'USER' },
-    ],
-    headerAlign: 'center',
-    align: 'center',
-    editable: true,
-    flex: 1,
-  },
-  {
-    field: 'created',
-    headerName: 'Created',
-    description: 'When the user was created',
-    type: 'dateTime',
-    flex: 1,
-    headerAlign: 'center',
-    align: 'center',
-    valueGetter: (params: GridValueGetterParams<User, string>) =>
-      dayjs(params.value).toDate(),
-  },
-]
+import { DeleteUserCell } from './DeleteUserCell'
 
 interface UsersTableProps {
   setUsers: Dispatch<SetStateAction<User[] | undefined>>
@@ -64,14 +28,74 @@ export const UsersTable: React.FC<UsersTableProps> = (props) => {
     oldUser: User
   ) => updateUser(oldUser.username, newUser)
 
+  /** Columns for the user managmement table */
+  const columns: GridColDef<User, any, User>[] = [
+    { field: 'username', headerName: 'Username', flex: 1 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      editable: true,
+      flex: 1,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      editable: true,
+      flex: 1,
+    },
+    {
+      field: 'role',
+      headerName: 'Role',
+      type: 'singleSelect',
+      valueOptions: [
+        { value: UserRole.ADMIN, label: 'ADMIN' },
+        { value: UserRole.GARDENER, label: 'GARDENER' },
+        { value: UserRole.USER, label: 'USER' },
+      ],
+      headerAlign: 'center',
+      align: 'center',
+      editable: true,
+      flex: 1,
+    },
+    {
+      field: 'created',
+      headerName: 'Created',
+      description: 'When the user was created',
+      type: 'dateTime',
+      flex: 1,
+      headerAlign: 'center',
+      align: 'center',
+      valueGetter: (params: GridValueGetterParams<User, string>) =>
+        dayjs(params.value).toDate(),
+    },
+    {
+      field: 'action',
+      headerName: 'Delete',
+      description: 'Delete the given user',
+      headerAlign: 'center',
+      align: 'center',
+      sortable: false,
+      renderCell: (params: GridRenderCellParams<User, any, User>) => {
+        return (
+          <DeleteUserCell
+            setUsers={props.setUsers}
+            username={params.row.username}
+          />
+        )
+      },
+    },
+  ]
+
   return (
-    <DataGrid<User, any, User>
-      columns={columns}
-      getRowId={(row: User) => row.username}
-      processRowUpdate={handleUpdateUser}
-      rows={props.users}
-      setRows={props.setUsers}
-      fetchRows={getUsers}
-    />
+    <>
+      <DataGrid<User, any, User>
+        columns={columns}
+        getRowId={(row: User) => row.username}
+        processRowUpdate={handleUpdateUser}
+        rows={props.users}
+        setRows={props.setUsers}
+        fetchRows={getUsers}
+      />
+    </>
   )
 }
