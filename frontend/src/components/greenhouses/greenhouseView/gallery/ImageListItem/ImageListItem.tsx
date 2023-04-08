43b423a00aae0edba01e0f4ec85e5d5c05ca6d57
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-08 15:24:57 +0200

    Add ItemListBar to delete images

---
+++ frontend/src/components/greenhouses/greenhouseView/gallery/ImageListItem/ImageListItem.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+import MuiImageListItem from '@mui/material/ImageListItem'
+
+import { useUserRole } from '~/hooks/user'
+import { Image } from '~/models/image'
+import { UserRole } from '~/models/user'
+
+import { DeleteImageBar } from './DeleteImageBar'
+
+interface ImageListItemProps {
+  /** Image alt-text */
+  alt: string
+  /** URL to the images */
+  image: Image
+}
+
+/**
+ * Customised ImageList item to allow gardeners and admins to delete images
+ */
+export const ImageListItem: React.FC<ImageListItemProps> = (props) => {
+  const userRole = useUserRole()
+  const [showItemBar, setShowItemBar] = useState(false)
+
+  /** Show the overlay with a button to delete images if the user is a gardener or admin */
+  const handleShowItemBar = () => {
+    if ([UserRole.ADMIN, UserRole.GARDENER].includes(userRole)) {
+      setShowItemBar(true)
+    }
+  }
+
+  return (
+    <MuiImageListItem
+      key={props.image}
+      sx={{
+        background: 'blue',
+      }}
+      onMouseOver={handleShowItemBar}
+      onMouseOut={() => setShowItemBar(false)}
+    >
+      <img
+        src={`${props.image}?w=248&fit=crop&auto=format`}
+        srcSet={`${props.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+        alt={props.alt}
+        loading="lazy"
+      />
+      <DeleteImageBar image={props.image} show={showItemBar} />
+    </MuiImageListItem>
+  )
+}
