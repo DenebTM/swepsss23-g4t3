8b11956f7414acf53ffec6c741d09b14eb438db6
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-04-09 19:54:04 +0200

    Add function to update a single sensor station

--- frontend/src/api/endpoints/sensorStations/sensorStations.ts
+++ frontend/src/api/endpoints/sensorStations/sensorStations.ts
@@ -1,5 +1,5 @@
 import { Server } from 'miragejs'
-import { _delete, _get } from '~/api/intercepts'
+import { _delete, _get, _put } from '~/api/intercepts'
 import { SensorStation, SensorStationUuid } from '~/models/sensorStation'
 
 import { AppSchema, EndpointReg } from '../../mirageTypes'
@@ -18,7 +18,7 @@ export const getSensorStations = async (): Promise<SensorStation[]> => {
 
 /**
  * GET /api/sensor-stations/${uuid}
- * Get a single access point by ID
+ * Get a single sensor station by ID
  * @returns The fetched access point
  */
 export const getSensorStation = async (
@@ -29,13 +29,27 @@ export const getSensorStation = async (
 
 /**
  * DEL /api/sensor-stations/${uuid}
- * Delete a single access point by ID
+ * Delete a single sensor station  by ID
  */
 export const deleteSensorStation = async (
   sensorStationUuid: SensorStationUuid
 ): Promise<SensorStation> => {
   return _delete(`${SENSOR_STATIONS_URI}/${sensorStationUuid}`)
 }
+
+/**
+ * PUT /api/sensor-stations/${uuid}
+ * Update a single sensor station by ID
+ */
+export const updateSensorStation = async (
+  sensorStationUuid: SensorStationUuid,
+  newParams: Omit<Partial<SensorStation>, 'uuid'>
+): Promise<SensorStation> => {
+  return _put(`${SENSOR_STATIONS_URI}/${sensorStationUuid}`, newParams)
+}
+
+/** Path for mocked routes related to a single sensor station */
+const sensorStationPath = `${SENSOR_STATIONS_URI}/:uuid`
 
 /** Mocked sensor station functions */
 export const mockedSensorStationReqs: EndpointReg = (server: Server) => {
@@ -46,7 +60,7 @@ export const mockedSensorStationReqs: EndpointReg = (server: Server) => {
   })
 
   /** Mock {@link getSensorStation} */
-  server.get(`${SENSOR_STATIONS_URI}/:uuid`, (schema: AppSchema, request) => {
+  server.get(sensorStationPath, (schema: AppSchema, request) => {
     const uuid: SensorStationUuid = Number(request.params.uuid)
     const sensorStation = schema.findBy('sensorStation', { uuid: uuid })
 
@@ -56,18 +70,29 @@ export const mockedSensorStationReqs: EndpointReg = (server: Server) => {
   })
 
   /** Mock {@link deleteSensorStation} */
-  server.delete(
-    `${SENSOR_STATIONS_URI}/:uuid`,
-    (schema: AppSchema, request) => {
-      const uuid: SensorStationUuid = Number(request.params.uuid)
-      const sensorStation = schema.findBy('sensorStation', { uuid: uuid })
+  server.delete(sensorStationPath, (schema: AppSchema, request) => {
+    const uuid: SensorStationUuid = Number(request.params.uuid)
+    const sensorStation = schema.findBy('sensorStation', { uuid: uuid })
 
-      if (sensorStation) {
-        sensorStation.destroy()
-        return success()
-      } else {
-        return notFound(`sensor station ${uuid}`)
-      }
+    if (sensorStation) {
+      sensorStation.destroy()
+      return success()
+    } else {
+      return notFound(`sensor station ${uuid}`)
     }
-  )
+  })
+
+  /** Mock {@link updateSensorStation} */
+  server.put(sensorStationPath, (schema: AppSchema, request) => {
+    const uuid: SensorStationUuid = Number(request.params.uuid)
+    const sensorStation = schema.findBy('sensorStation', { uuid: uuid })
+    const newParams: Partial<SensorStation> = JSON.parse(request.requestBody)
+
+    if (sensorStation) {
+      sensorStation.update(newParams)
+      return success()
+    } else {
+      return notFound(`sensor station ${uuid}`)
+    }
+  })
 }
