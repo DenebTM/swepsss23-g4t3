2c5fdd1bfbf913adb02e768ebcfcd7199930541e
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-04-10 01:01:30 +0200

    Move pairing mode to its own file, add newlines at ends of files

---
+++ arduino/include/ble/pairing.h
@@ -0,0 +1,23 @@
+#ifndef _BLE_PAIR_H
+#define _BLE_PAIR_H
+
+#include "ble.h"
+
+namespace ble::pairing {
+  /** set up button */
+  void setup();
+
+  /** check for button press or connection requests */
+  void update();
+
+  namespace mode {
+    extern volatile bool entering;
+    extern bool active;
+    extern unsigned long timestamp;
+
+    void enter();
+    void exit();
+  }
+}
+
+#endif
