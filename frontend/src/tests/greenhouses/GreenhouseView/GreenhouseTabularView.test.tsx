54e93a4829198bc4feb3c1ced86276b2953bc404
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-10 23:04:01 +0200

    Add timeout to greenhouse tabular view test

--- frontend/src/tests/greenhouses/GreenhouseView/GreenhouseTabularView.test.tsx
+++ frontend/src/tests/greenhouses/GreenhouseView/GreenhouseTabularView.test.tsx
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { test, vi } from 'vitest'
 import {
   GREENHOUSE_VIEW_QUERY,
@@ -23,6 +23,12 @@ vi.mock('react-router-dom', () => ({
 
 test('render GreenhouseTabularView inside GreenhouseView without crashing', async () => {
   render(<GreenhouseView />)
-  expect(screen.getByText('Air Pressure')).toBeInTheDocument()
+  // Wait for mocked API call before table loads
+  await waitFor(
+    () => {
+      expect(screen.getByText('Air Pressure')).toBeInTheDocument()
+    },
+    { timeout: 4000 }
+  )
   // TODO qqjf Add tests for all table columns
 })
