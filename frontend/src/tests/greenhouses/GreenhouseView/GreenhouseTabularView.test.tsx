fb906541e599549953e7680f132e65b897c5be27
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-12 01:31:50 +0200

    Add test for greenhouse columns back in

--- frontend/src/tests/greenhouses/GreenhouseView/GreenhouseTabularView.test.tsx
+++ frontend/src/tests/greenhouses/GreenhouseView/GreenhouseTabularView.test.tsx
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { test, vi } from 'vitest'
 import {
   GREENHOUSE_VIEW_QUERY,
@@ -24,6 +24,6 @@ vi.mock('react-router-dom', () => ({
 
 test('render GreenhouseTabularView inside GreenhouseView without crashing', async () => {
   render(<GreenhouseView />)
-  // qqjf causes test to hang expect(screen.getByText('Air Pressure')).toBeInTheDocument()
+  expect(screen.getByText('Air Pressure')).toBeInTheDocument()
   // TODO qqjf Add tests for all table columns
 })
