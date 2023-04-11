724b1f672149431016ef2375cd08cf32a858f90c
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-12 01:18:46 +0200

    Make range display cell width clearer

--- frontend/src/components/greenhouses/myGreenhouses/GreenhouseAccordion/GreenhouseAccordionContents/EditableCell/RangeDisplayCell.tsx
+++ frontend/src/components/greenhouses/myGreenhouses/GreenhouseAccordion/GreenhouseAccordionContents/EditableCell/RangeDisplayCell.tsx
@@ -18,21 +18,18 @@ interface RangeDisplayCellProps {
  * Display the current range of an editable greenhouse metric range.
  */
 export const RangeDisplayCell: React.FC<RangeDisplayCellProps> = (props) => {
-  const rangeValueWidth = '42%'
-
   return (
     <>
       <Typography
         {...props.typographyProps}
         align="right"
         display="inline-block"
-        width={rangeValueWidth}
       >
         {props.value.lower.toFixed(1)} {props.unit}
       </Typography>
       <Typography
         {...props.typographyProps}
-        sx={{ margin: theme.spacing(0, 1) }}
+        sx={{ margin: theme.spacing(0, 1), width: '15%' }}
       >
         &#8212;
       </Typography>
@@ -40,7 +37,6 @@ export const RangeDisplayCell: React.FC<RangeDisplayCellProps> = (props) => {
         {...props.typographyProps}
         align="left"
         display="inline-block"
-        width={rangeValueWidth}
       >
         {props.value.upper.toFixed(1)} {props.unit}
       </Typography>
