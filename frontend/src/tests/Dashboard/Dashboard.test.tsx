6c92b654f2091b85374b275cbb77809911656b01
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-04-12 16:07:25 +0200

    Improve dashboard tests

--- frontend/src/tests/Dashboard/Dashboard.test.tsx
+++ frontend/src/tests/Dashboard/Dashboard.test.tsx
@@ -1,23 +1,21 @@
-import {
-  render,
-  /*, screen*/
-} from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { test } from 'vitest'
-
-/*import {
+import {
   GREENHOUSE_METRICS,
   GreenhouseMetricRange,
   greenhouseMetricWithUnit,
-} from '~/common'*/
+} from '~/common'
 import { Dashboard } from '~/components/dashboard/Dashboard'
 
 test('render Dashboard without crashing', async () => {
   render(<Dashboard />)
 
-  // Expect a column for every metric to be visible
-  /*GREENHOUSE_METRICS.forEach((metricRange: GreenhouseMetricRange) =>
-    expect(
-      screen.getByText(greenhouseMetricWithUnit(metricRange))
-    ).toBeInTheDocument()
-  )*/
+  // Expect a column for every metric to be visible once the measurements are fetched from the API
+  GREENHOUSE_METRICS.forEach((metricRange: GreenhouseMetricRange) =>
+    waitFor(() =>
+      expect(
+        screen.getByText(greenhouseMetricWithUnit(metricRange))
+      ).toBeInTheDocument()
+    )
+  )
 })
