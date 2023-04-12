0479cd254e3d77111dc7f2bb85426980fee919b4
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-12 16:00:40 +0200

    Add dashboard table showing current measurements (if any)

--- frontend/src/tests/Dashboard/Dashboard.test.tsx
+++ frontend/src/tests/Dashboard/Dashboard.test.tsx
@@ -1,7 +1,23 @@
-import { render } from '@testing-library/react'
+import {
+  render,
+  /*, screen*/
+} from '@testing-library/react'
 import { test } from 'vitest'
+
+/*import {
+  GREENHOUSE_METRICS,
+  GreenhouseMetricRange,
+  greenhouseMetricWithUnit,
+} from '~/common'*/
 import { Dashboard } from '~/components/dashboard/Dashboard'
 
 test('render Dashboard without crashing', async () => {
   render(<Dashboard />)
+
+  // Expect a column for every metric to be visible
+  /*GREENHOUSE_METRICS.forEach((metricRange: GreenhouseMetricRange) =>
+    expect(
+      screen.getByText(greenhouseMetricWithUnit(metricRange))
+    ).toBeInTheDocument()
+  )*/
 })
