5c78d5745b6c48f04cd11b21b82c09f978afd43e
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-04-14 00:16:17 +0200

    Add initial line graph for greenhouse measurements

--- frontend/src/components/greenhouses/greenhouseView/graphical/GreenhouseGraph.tsx
+++ frontend/src/components/greenhouses/greenhouseView/graphical/GreenhouseGraph.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+  CartesianGrid,
+  Label,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
 
+import dayjs from 'dayjs'
 import { Measurement } from '~/models/measurement'
 import { SensorStationUuid } from '~/models/sensorStation'
+import { theme } from '~/styles/theme'
+
+interface DataValue {
+  name: string // Timestamp
+  humidity: number
+  soilMoisture: number
+}
 
 interface GreenhouseGraphProps {
   measurements: Measurement[]
@@ -12,9 +31,49 @@ interface GreenhouseGraphProps {
  * Graph showing recent greenhouse sensor data
  */
 export const GreenhouseGraph: React.FC<GreenhouseGraphProps> = (props) => {
+  const [data, setData] = useState<DataValue[]>()
+
+  useEffect(() => {
+    const dataVals: DataValue[] = props.measurements
+      .map((m) => ({
+        name: dayjs(m.timestamp).format('YYYY-MM-DD'),
+        humidity: m.data.humidity,
+        soilMoisture: m.data.soilMoisture,
+      }))
+      .sort((a, b) => (a.name > b.name ? 1 : -1))
+
+    setData(dataVals)
+  }, [props.measurements])
+
   return (
-    <div>
-      Greenhouse Graph: {JSON.stringify(props.measurements.slice(0, 5))}
-    </div>
+    <>
+      {data && (
+        <ResponsiveContainer width="100%" aspect={3}>
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis domain={[0, 100]}>
+              <Label angle={-90} position="center" fill={theme.outline}>
+                Percentile
+              </Label>
+            </YAxis>
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="humidity" stroke="#8884d8" />
+            <Line type="monotone" dataKey="soilMoisture" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+      )}
+    </>
   )
 }
