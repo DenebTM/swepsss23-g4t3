823f6ccafb038f3c741eed490a0b4e90db23ea6f
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-04-13 22:49:45 +0200

    Allow setting more than one hardware timer

--- arduino/src/hwtimer.cpp
+++ arduino/src/hwtimer.cpp
@@ -1,15 +1,26 @@
 #include <hwtimer.h>
 
 #include <NRF52_MBED_TimerInterrupt_Generic.h>
+#include <ISR_Timer_Generic.h>
+
+#define INTERNAL_TIMER_INTERVAL_US 1000
 
 namespace hwtimer {
-  NRF52_MBED_Timer timer(NRF_TIMER_4);
+  NRF52_MBED_Timer internal_timer(NRF_TIMER_4);
+  ISR_Timer multiplexed_timer;
+  bool timer_setup = false;
 
-  /**
-   * this function exists to abstract away the internal timer object, and set
-   * the interval in milli- instead of microseconds
-   */
-  bool attach_isr(unsigned int interval_ms, voidFuncPtr callback) {
-    return timer.attachInterruptInterval(interval_ms * 1000, callback);
+  int set_interval(unsigned int interval, voidFuncPtr callback) {
+    // Enable hardware timer upon first time setting an interval
+    if (!timer_setup) {
+      if (!internal_timer.attachInterruptInterval(INTERNAL_TIMER_INTERVAL_US,
+        []() { multiplexed_timer.run(); })) {
+          return -1;
+      }
+
+      timer_setup = true;
+    }
+
+    return multiplexed_timer.setInterval(interval, callback);
   }
 }
