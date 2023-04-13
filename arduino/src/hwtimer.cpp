046cee6e12d835263f637caa4e09a5c32f53bafe
DenebTM <deneb@screee.ee>
2023-04-13 23:59:48 +0200

    Switch to mbed::LowPowerTicker

--- arduino/src/hwtimer.cpp
+++ arduino/src/hwtimer.cpp
@@ -1,26 +1,18 @@
 #include <hwtimer.h>
 
-#include <NRF52_MBED_TimerInterrupt_Generic.h>
-#include <ISR_Timer_Generic.h>
+#include <drivers/LowPowerTicker.h>
+#include <vector>
+#include <chrono>
+using namespace std::chrono_literals;
 
 #define INTERNAL_TIMER_INTERVAL_US 1000
 
 namespace hwtimer {
-  NRF52_MBED_Timer internal_timer(NRF_TIMER_4);
-  ISR_Timer multiplexed_timer;
-  bool timer_setup = false;
+  std::vector<mbed::LowPowerTicker*> tickers;
 
-  int set_interval(unsigned int interval, voidFuncPtr callback) {
-    // Enable hardware timer upon first time setting an interval
-    if (!timer_setup) {
-      if (!internal_timer.attachInterruptInterval(INTERNAL_TIMER_INTERVAL_US,
-        []() { multiplexed_timer.run(); })) {
-          return -1;
-      }
-
-      timer_setup = true;
-    }
-
-    return multiplexed_timer.setInterval(interval, callback);
+  void set_interval(unsigned int interval, mbed::Callback<void()> callback) {
+    mbed::LowPowerTicker *t = new mbed::LowPowerTicker();
+    t->attach(callback, interval * 1ms);
+    tickers.push_back(t);
   }
 }
