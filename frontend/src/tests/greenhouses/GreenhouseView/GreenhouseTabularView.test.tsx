117745dc0790975bdadfea375147d0c66e60053d
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-14 13:33:01 +0200

    Add test for greenhouse table with mocked data

--- frontend/src/tests/greenhouses/GreenhouseView/GreenhouseTabularView.test.tsx
+++ frontend/src/tests/greenhouses/GreenhouseView/GreenhouseTabularView.test.tsx
@@ -5,23 +5,64 @@ import {
   GREENHOUSE_VIEW_QUERY,
   GreenhouseMetricRange,
   greenhouseMetricWithUnit,
+  roundMetric,
   SensorStationView,
   SS_UUID_PARAM,
 } from '~/common'
 import { GreenhouseView } from '~/components/greenhouses/greenhouseView/GreenhouseView'
+import { Measurement } from '~/models/measurement'
+import { SensorStationUuid } from '~/models/sensorStation'
+import { Timestamp } from '~/models/timestamp'
 
-/** Mocked UUID for testing. Should have meaurements defined. */
-const sensorStationUuid = 0
+/** Mocked UUID for testing. */
+const SENSOR_STATION_UUID = 0
+
+/** Mocked {@link Measurement}s for testing */
+const MOCKED_MEASUREMENTS: Measurement[] = [
+  {
+    id: 17,
+    timestamp: '2023-03-30T15:16:52.317Z',
+    data: {
+      temperature: 55.028911,
+      soilMoisture: 52.309543,
+      lightIntensity: 256.282546,
+      humidity: 72.350015,
+      airQuality: 181.558768,
+      airPressure: 826.347872,
+    },
+  },
+  {
+    id: 22,
+    timestamp: '2023-04-10T14:43:27.906Z',
+    data: {
+      temperature: 7.911702,
+      soilMoisture: 38.300971,
+      lightIntensity: 842.59735,
+      humidity: 38.565525,
+      airQuality: 35.658449,
+      airPressure: 1152.702981,
+    },
+  },
+]
 
 /** Mock passing in the view via search params */
 vi.mock('react-router-dom', () => ({
   useSearchParams: (): [URLSearchParams] => [
     new URLSearchParams([[GREENHOUSE_VIEW_QUERY, SensorStationView.TABLE]]),
   ],
-  useParams: () => ({ [SS_UUID_PARAM]: sensorStationUuid }),
+  useParams: () => ({ [SS_UUID_PARAM]: SENSOR_STATION_UUID }),
   useNavigate: () => vi.fn(),
   useLocation: () => vi.fn(),
   useRouteError: () => vi.fn(),
+}))
+
+/** Mock {@link getSensorStationMeasurements} for testing */
+vi.mock('~/api/endpoints/sensorStations/measurements', () => ({
+  getSensorStationMeasurements: (
+    sensorStationUuid: SensorStationUuid,
+    from?: Timestamp,
+    to?: Timestamp
+  ) => Promise.resolve(MOCKED_MEASUREMENTS),
 }))
 
 test('render GreenhouseTabularView inside GreenhouseView', async () => {
@@ -33,6 +74,15 @@ test('render GreenhouseTabularView inside GreenhouseView', async () => {
       expect(
         screen.getByText(greenhouseMetricWithUnit(metricRange))
       ).toBeInTheDocument()
+    )
+  )
+
+  // Expect (rounded) mocked data values to be visible in table
+  MOCKED_MEASUREMENTS.forEach((measurement: Measurement) =>
+    Object.values(measurement.data).forEach((value: number) =>
+      waitFor(() =>
+        expect(screen.getByText(roundMetric(value))).toBeInTheDocument()
+      )
     )
   )
 })
