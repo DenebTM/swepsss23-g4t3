baf62ce5f709e3af14d78bdeccb2f4598924972a
Luana Bach Gadelha <luana.bach-gadelha@student.uibk.ac.at>
2023-04-14 19:21:34 +0200

    added delete request for photos

--- src/main/java/at/qe/skeleton/controllers/ImageDataController.java
+++ src/main/java/at/qe/skeleton/controllers/ImageDataController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 public class ImageDataController {
@@ -26,12 +29,22 @@ public class ImageDataController {
                 .getId();
     }
 
-    @GetMapping(value = "/image/{imageId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    @GetMapping(value = "/photo/{imageId}", produces = MediaType.IMAGE_JPEG_VALUE)
     ByteArrayResource downloadImage(@PathVariable Integer imageId) {
         byte[] image = imageDbRepository.findById(imageId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))
                 .getContent();
 
         return new ByteArrayResource(image);
+    }
+
+    @DeleteMapping(value = "/photo/{imageId}")
+    ResponseEntity<Object> deletePhoto(@PathVariable Integer imageId) {
+        Optional<ImageData> maybeImage = imageDbRepository.findById(imageId);
+        if (maybeImage.isPresent()) {
+            imageDbRepository.delete(maybeImage.get());
+            return ResponseEntity.ok(maybeImage.get());
+        }
+        return ResponseEntity.ofNullable(maybeImage);
     }
 }
