054c3ac69002c44468d8162ad71f138ee379b2eb
REDACTED-4 <REDACTED-4@student.uibk.ac.at>
2023-04-14 19:21:34 +0200

    added delete request for photos

--- src/main/java/at/qe/skeleton/repositories/ImageDataRepository.java
+++ src/main/java/at/qe/skeleton/repositories/ImageDataRepository.java
@@ -1,6 +1,7 @@
 package at.qe.skeleton.repositories;
 
 import at.qe.skeleton.model.ImageData;
+import at.qe.skeleton.model.SensorStation;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,6 +9,9 @@ import java.util.Optional;
 @Repository
 public interface ImageDataRepository extends AbstractRepository<ImageData, Integer> {
 
-
     Optional<ImageData> findByName(String name);
+
+    Optional<ImageData> findById(Integer id);
+
+    Optional<ImageData> findBySensorStation(SensorStation sensorStation);
 }
