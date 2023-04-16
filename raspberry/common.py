62100745e0e87bf6268201224400cd2ecab06e7d
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-04-16 21:56:06 +0200

    fix UUIDs

--- raspberry/common.py
+++ raspberry/common.py
@@ -6,12 +6,12 @@ sensor_station_name = "PH SensorStation"
 
 # global constants taken out of the BLE Communication Spec
 base_uuid = "0000{}-0000-1000-8000-00805f9b34fb"
+air_pressure_uuid = base_uuid.format("2a6d")
 temperature_uuid = base_uuid.format("2a6e")
 humidity_uuid = base_uuid.format("2a6f")
-air_pressure_uuid = base_uuid.format("2a6d")
-illuminance_uuid = base_uuid.format("ff04")
-air_quality_index_uuid = base_uuid.format("ff05")
-soil_moisture_uuid = base_uuid.format("ff06")
+illuminance_uuid = base_uuid.format("2afb")
+air_quality_index_uuid = base_uuid.format("f105")
+soil_moisture_uuid = base_uuid.format("f106")
 
 # global constants for the error messages takne out of the ble communication Spec
 error_service_uuid = base_uuid.format("ff00")
