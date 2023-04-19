7ec3f4b2b744b081cab8fe05c32cc5c610920894
REDACTED-3 <REDACTED-3@student.uibk.ac.at>
2023-04-19 12:33:04 +0200

    added dependencies for mockito and class with empty methods to test user controllers

---
+++ src/test/java/at/qe/skeleton/controllers/api/UserxRestControllerTest.java
@@ -0,0 +1,38 @@
+package at.qe.skeleton.controllers.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@WebAppConfiguration
+@ExtendWith(MockitoExtension.class)
+class UserxRestControllerTest {
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void getUserByUsername() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUserByUsername() {
+    }
+
+    @Test
+    void getAssignedSS() {
+    }
+}
