c5056b470cf90a163aef63ea795a44ba72951782
DenebTM <deneb@screee.ee>
2023-04-20 22:13:18 +0200

    Break code on purpose

--- arduino/src/led.cpp
+++ arduino/src/led.cpp
@@ -11,7 +11,7 @@ void led::set_color(Color color) {
   uint8_t green = (color & 0x00FF00) >>  8;
   uint8_t blue  = (color & 0x0000FF) >>  0;
 
-  analogWrite(LED_RED_PIN, red);
+  analogWrite(LED_RED_PIN, red)
   analogWrite(LED_GREEN_PIN, green);
   analogWrite(LED_BLUE_PIN, blue);
 
