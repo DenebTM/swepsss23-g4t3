4bf9b74925179ff22a6f39919c58d8348e0bd0fe
REDACTED-3 <REDACTED-3@student.uibk.ac.at>
2023-04-20 16:56:03 +0200

    added @Repository Bean in repositorys

--- src/main/java/at/qe/skeleton/repositories/MeasurementRepository.java
+++ src/main/java/at/qe/skeleton/repositories/MeasurementRepository.java
@@ -1,7 +1,9 @@
 package at.qe.skeleton.repositories;
 
 import at.qe.skeleton.models.Measurement;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MeasurementRepository extends AbstractRepository<Measurement, Long> {
 
     Measurement findFirstById(Integer id);
