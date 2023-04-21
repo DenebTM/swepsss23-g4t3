62fb59d1555f913e8ff17e20853d662fde81bc72
REDACTED-4 <REDACTED-4@student.uibk.ac.at>
2023-04-21 08:36:31 +0200

    empty db entities for default logback tables

---
+++ src/main/java/at/qe/skeleton/models/LoggingEventException.java
@@ -0,0 +1,7 @@
+package at.qe.skeleton.models;
+
+import jakarta.persistence.Table;
+
+@Table(name = "logging_event_exception")
+public class LoggingEventException {
+}
---
+++ src/main/java/at/qe/skeleton/models/LoggingEventProperty.java
@@ -0,0 +1,7 @@
+package at.qe.skeleton.models;
+
+import jakarta.persistence.Table;
+
+@Table(name = "logging_event_property")
+public class LoggingEventProperty {
+}
