341435593ac5e6b4b558b09c159889679c3e59e7
REDACTED-4 <REDACTED-4@student.uibk.ac.at>
2023-04-22 13:18:02 +0200

    filled logback default table LoggingEventException

--- src/main/java/at/qe/skeleton/models/LoggingEventException.java
+++ src/main/java/at/qe/skeleton/models/LoggingEventException.java
@@ -1,7 +1,27 @@
 package at.qe.skeleton.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
+@IdClass(LoggingEvent.class)
 @Table(name = "logging_event_exception")
 public class LoggingEventException {
+
+    @Id
+    @Column(name = "event_id", nullable = false)
+    @JdbcTypeCode(SqlTypes.BIGINT)
+    private Long eventId;
+
+    @Id
+    @Column(name = "mapped_key", nullable = false)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private String mappedKey;
+
+    @Column(name = "mapped_value")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private String mappedValue;
 }
