6748e0d035585bf4563288e2a75b2a9d964b7a23
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-04-22 23:40:18 +0200

    Update LED status codes as per wiki page

--- arduino/include/ble/pairing.h
+++ arduino/include/ble/pairing.h
@@ -9,8 +9,7 @@ using namespace std::chrono_literals;
 #define BLE_PAIRING_MODE_TIMEOUT_MS   5 * 60 * 1000
 
 static led::StatusCode* const LEDC_BLE_UNPAIRED = new led::StatusCode{
-  { led::Color::RED, 1500ms },
-  { led::Color::OFF, 1500ms },
+  { led::Color::RED, 100ms },
 };
 static led::StatusCode* const LEDC_BLE_PAIRING = new led::StatusCode{
   { led::Color::BLUE, 250ms },
@@ -20,7 +19,7 @@ static led::StatusCode* const LEDC_BLE_CONNECTED = new led::StatusCode{
   { led::Color::GREEN, 100ms }
 };
 static led::StatusCode* const LEDC_BLE_DISCONNECTED = new led::StatusCode{
-  { led::Color::YELLOW, 1s },
+  { led::Color::RED, 1s },
   { led::Color::OFF, 1s },
 };
 
