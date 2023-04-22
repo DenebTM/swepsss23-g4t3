2a4fcd6d81b0144c00c7283d22c1666b379e1b4d
DenebTM <deneb@screee.ee>
2023-04-22 23:40:18 +0200

    Update LED status codes as per wiki page

--- arduino/src/led.cpp
+++ arduino/src/led.cpp
@@ -30,14 +30,15 @@ namespace led {
     pinMode(LED_BLUE_PIN, OUTPUT);
   }
 
-  Color active_color = OFF;
   void set_color(Color color) {
-    if (color == active_color) return;
+    static Color active_color;
+
+    if (active_color == color) return;
     active_color = color;
 
-    uint8_t red   = (color & 0xFF0000) >> 16;
-    uint8_t green = (color & 0x00FF00) >>  8;
-    uint8_t blue  = (color & 0x0000FF) >>  0;
+    uint8_t red   = (active_color & 0xFF0000) >> 16;
+    uint8_t green = (active_color & 0x00FF00) >>  8;
+    uint8_t blue  = (active_color & 0x0000FF) >>  0;
 
     analogWrite(LED_RED_PIN, red);
     analogWrite(LED_GREEN_PIN, green);
