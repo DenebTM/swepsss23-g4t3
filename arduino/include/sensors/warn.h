b8c14c7adaebf75b07d4bfe9654760c55268aba5
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-04-25 19:41:05 +0200

    Add warn code constants

--- arduino/include/sensors/warn.h
+++ arduino/include/sensors/warn.h
@@ -2,6 +2,68 @@
 #define _SENSORS_WARN_H
 
 #include <Arduino.h>
+#include <led.h>
+
+static led::StatusCode* const LEDC_WARN_AIR_PRESSURE = new led::StatusCode{
+  { led::Color::YELLOW, 500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::YELLOW, 500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::YELLOW, 500ms },
+  { led::Color::OFF,    500ms },
+};
+static led::StatusCode* const LEDC_WARN_TEMPERATURE = new led::StatusCode{
+  { led::Color::YELLOW, 200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::YELLOW, 200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::YELLOW, 200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::YELLOW, 500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::YELLOW, 500ms },
+  { led::Color::OFF,    500ms },
+};
+static led::StatusCode* const LEDC_WARN_ILLUMINANCE = new led::StatusCode{
+  { led::Color::PURPLE, 500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::PURPLE, 500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::PURPLE, 500ms },
+  { led::Color::OFF,    500ms },
+};
+static led::StatusCode* const LEDC_WARN_AIR_QUALITY = new led::StatusCode{
+  { led::Color::PURPLE, 200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::PURPLE, 200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::PURPLE, 200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::PURPLE, 500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::PURPLE, 500ms },
+  { led::Color::OFF,    500ms },
+};
+static led::StatusCode* const LEDC_WARN_HUMIDITY = new led::StatusCode{
+  { led::Color::CYAN,   500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::CYAN,   500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::CYAN,   500ms },
+  { led::Color::OFF,    500ms },
+};
+static led::StatusCode* const LEDC_WARN_SOIL_MOISTURE = new led::StatusCode{
+  { led::Color::CYAN,   200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::CYAN,   200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::CYAN,   200ms },
+  { led::Color::OFF,    200ms },
+  { led::Color::CYAN,   500ms },
+  { led::Color::OFF,    500ms },
+  { led::Color::CYAN,   500ms },
+  { led::Color::OFF,    500ms },
+};
 
 namespace sensors {
   struct sensor_warnings {
