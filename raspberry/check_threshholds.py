636237ef23175e2fc7aa729f893c4513c1fcaa2c
csaw5791 <Andreas.Schett@student.uibk.ac.at>
2023-04-27 19:28:23 +0200

    made functionality better with dictionary instead of ifelse in check_threshholds.py

--- raspberry/check_threshholds.py
+++ raspberry/check_threshholds.py
@@ -7,35 +7,25 @@ async def check_values_for_thresholds(sensorstationClient, averages_dict, thresh
                     max_threshold = thresholds_dict[sensor+"_max"]
                     min_threshold = thresholds_dict[sensor+"_min"]
                     if max_threshold is not None and average_value > max_threshold or average_value < min_threshold:
-                         sendErrorToSensorstation(sensorstationClient, sensor)
-                         sendErrorToBackend(sensorstationClient, sensor)
+                         send_error_to_sensorstation(sensorstationClient, sensor)
+                         send_error_to_backend(sensorstationClient, sensor)
    
                         
-async def sendErrorToSensorstation(sensorstationClient, sensor):
+async def send_error_to_sensorstation(sensor_station_client, sensor):
     errorCode = 1
     errorCodeByteArray = errorCode.to_bytes(1, byteorder='little')
     try:
-        if sensor == "air_pressure":
-            await sensorstationClient.write_gatt_char(common.air_quality_failure_uuid, errorCodeByteArray)
-        if sensor == "temperature":
-            await sensorstationClient.write_gatt_char(common.temperature_failure_uuid, errorCodeByteArray)
-        elif sensor == "humidity":
-            await sensorstationClient.write_gatt_char(common.humidity_failure_uuid, errorCodeByteArray)
-        elif sensor == "illuminance":
-            await sensorstationClient.write_gatt_char(common.illuminance_failure_uuid, errorCodeByteArray)
-        elif sensor == "air_quality":
-            await sensorstationClient.write_gatt_char(common.air_quality_failure_uuid, errorCodeByteArray)
-        elif sensor == "soil_moisture":
-            await sensorstationClient.write_gatt_char(common.soil_moisture_failure_uuid, errorCodeByteArray)
+        sensor_uuid = common.sensor_uuids[sensor]
+        await sensor_station_client.write_gatt_char(sensor_uuid, errorCodeByteArray)
     except:
          print("couldnt write to gatt todo log")
     #TODO log error code
 
 
-async def sendErrorToBackend(sensorstationClient, sensor):
+async def send_error_to_backend(sensor_station_client, sensor):
     try:
-        data = {'sensorstation': sensorstationClient.name, 'sensor': sensor}
-        response = requests.post("idk address of errors", json=data)
+        data = {'sensorstation': sensor_station_client.name, 'sensor': sensor}
+        requests.post("idk address of errors", json=data)
     except:
         print("couldnt connect to backend or smth")
     
