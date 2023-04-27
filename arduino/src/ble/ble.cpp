5d9e2fc8681b5bb0852275258afeb0a0b0580b2b
DenebTM <deneb@screee.ee>
2023-04-27 18:31:34 +0200

    clear active sensor warnings after pairing

--- arduino/src/ble/ble.cpp
+++ arduino/src/ble/ble.cpp
@@ -7,6 +7,7 @@
 #include <buttons.h>
 #include <led.h>
 #include <sensors/data.h>
+#include <sensors/warn.h>
 
 namespace ble {
   String paired_mac = BLE_NO_PAIRED_DEVICE;
@@ -15,11 +16,15 @@ namespace ble {
     String new_mac = central.address();
 
     // currently in pairing mode; pair with connecting access point
+    // and clear currently active sensor warnings
     if (pairing::mode::active) {
       paired_mac = new_mac;
 
       Serial.print("Paired with access point: ");
       Serial.println(paired_mac);
+
+      // clear all currently active warnings
+      memset(&sensors::current_warnings, 0, sizeof sensors::current_warnings);
 
       pairing::mode::active = false;
       led::set_status_code(LEDC_BLE_CONNECTED);
