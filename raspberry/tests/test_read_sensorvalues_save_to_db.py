91055699190d8c1621134e903bf9fcacc087c062
csaw5791 <Andreas.Schett@student.uibk.ac.at>
2023-04-27 19:23:26 +0200

    switched entirely to snakecase

--- raspberry/tests/test_read_sensorvalues_save_to_db.py
+++ raspberry/tests/test_read_sensorvalues_save_to_db.py
@@ -5,14 +5,14 @@ from bleak import BleakError
 
 
 from read_sensorvalues import read_sensorvalues
-from database_operations import saveSensorValuesToDatabase
+from database_operations import save_sensor_values_to_database
 
 
-class TestReadSensorValues(unittest.IsolatedAsyncioTestCase):
+class test_read_sensor_values(unittest.IsolatedAsyncioTestCase):
 
-    @patch('read_sensorvalues.saveSensorValuesToDatabase')
+    @patch('read_sensorvalues.save_sensor_values_to_database')
     @patch('read_sensorvalues.BleakClient')
-    async def test_read_sensorvalues(self, BleakClient, saveSensorValuesToDatabase):
+    async def test_read_sensorvalues(self, BleakClient, save_sensor_values_to_database):
 
         #modify return value of gatt char to 10
         BleakClient().__aenter__.return_value.read_gatt_char.return_value=int(10).to_bytes(2,byteorder='little')
@@ -21,7 +21,7 @@ class TestReadSensorValues(unittest.IsolatedAsyncioTestCase):
         await read_sensorvalues(BleakClient)
 
         # assert that saveSensorValuesToDatabase was called once with the expected arguments
-        saveSensorValuesToDatabase.assert_called_once_with(BleakClient.name, 10, 10, 10, 10, 10, 10)
+        save_sensor_values_to_database.assert_called_once_with(BleakClient.name, 10, 10, 10, 10, 10, 10)
 
 #TODO: implement test for logging when something is down
     # @patch('read_sensorvalues.BleakClient')
