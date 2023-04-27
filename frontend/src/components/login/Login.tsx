62c62a6d78f9f42279c98788be2d52935e8800a3
Jamie Fox <james.fox@student.uibk.ac.at>
2023-04-27 21:29:51 +0200

    Add responsive layout to login page

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -2,49 +2,50 @@ import React from 'react'
 
 import Container from '@mui/material/Container'
 import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/system/Box'
+import Grid from '@mui/material/Unstable_Grid2'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
-import { PlantIcon } from '@component-lib/PlantIcon'
 import { PAGE_URL } from '~/common'
 import { PageWrapper } from '~/components/page/PageWrapper'
 import { theme } from '~/styles/theme'
 
 import { GalleryCta } from './GalleryCta'
 import { LoginForm } from './LoginForm'
+import { LoginHeader } from './LoginHeader'
+import { LoginSidewave } from './LoginSidewave'
 
 /**
  * Login page
  */
 export const Login: React.FC = () => {
+  const narrowScreen = useMediaQuery(theme.breakpoints.down('md'))
+
   return (
     <PageWrapper hideSidebar permittedRoles={PAGE_URL.login.permittedRoles}>
-      <Container maxWidth="sm" sx={{ paddingTop: 10 }} disableGutters>
-        <Paper
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            padding: 4,
-          }}
-        >
-          <Box component="div" display="flex" alignItems="center" padding={2}>
-            <Typography
-              variant="headlineLarge"
-              align="center"
-              color="onSurface"
-              component="h1"
-              marginRight={1}
+      <Grid container spacing={2} padding={2}>
+        {!narrowScreen && (
+          <Grid xs={12} md={4}>
+            <LoginSidewave />
+          </Grid>
+        )}
+        <Grid xs={12} md={8}>
+          <Container maxWidth="sm" sx={{ paddingTop: 10 }} disableGutters>
+            <Paper
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: 4,
+              }}
             >
-              Log in to PlantHealth
-            </Typography>
-            <PlantIcon color={theme.onSurface} />
-          </Box>
-          <LoginForm />
+              <LoginHeader />
+              <LoginForm />
 
-          <GalleryCta />
-        </Paper>
-      </Container>
+              <GalleryCta />
+            </Paper>
+          </Container>
+        </Grid>
+      </Grid>
     </PageWrapper>
   )
 }
---
+++ frontend/src/components/login/LoginSidewave.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+/**
+ * Sidebar wave compnnet to display next to the login form on wider screens
+ */
+export const LoginSidewave: React.FC = () => {
+  return <>SIDEBAR</>
+}
