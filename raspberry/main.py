592ce223c9cd25132e10558ce08eca09c963f9ec
REDACTED-2 <REDACTED-2@student.uibk.ac.at>
2023-04-27 15:17:55 +0200

    add empty functions for asyncstructure

--- raspberry/main.py
+++ raspberry/main.py
@@ -10,12 +10,13 @@ from read_sensorvalues import read_sensorvalues
 from db import db_conn
 from search_for_sensorstations import search_for_sensorstations
 
-
-async def poll_for_connection():
+#amal alles aiohttp machen weil nid mischen
+#define den return als a future dass i des im manage sensorstations sieh
+async def poll_for_connection(connection_request):
     try:
         response = requests.get(common.web_server_address+"/access-points/"+common.access_point_name)
-
         return response.json['status']
+        asyncio.sleep(50)
     except:
         print("cant connect to webserver log")
 
@@ -24,16 +25,28 @@ async def spawn_sensorstation_tasks(sensorstations):
     for sensorstation in sensorstations:
         await asyncio.create_task(read_sensorvalues(sensorstation))
 
+async def cancel_sensorstation_tasks(sensorstation, sensorstation_tasks):
+    for sensorstation in sensorstation:
+        pass #TODO cancel tasks marked as offline
 
+async def manage_sensorstations(sensorstations, connection_request):
+    while not connection_request.done():
+        pass
+
+
+
+
+
+# immer neue future createn when einmal offline
 async def main():
     response = requests.post(common.web_server_address + "/accesspoints")
     while response.status_code != 200:
         response = requests.post(common.web_server_address + "/accesspoints")
 
-    while True:
         sensorstations = await search_for_sensorstations()
 
-        await spawn_sensorstation_tasks(sensorstations)
+
+        #await spawn_sensorstation_tasks(sensorstations)
     #     # send to /accesspoints that i exist POST
     #     access_point = {'name': common.access_point_name}
     #     try:
