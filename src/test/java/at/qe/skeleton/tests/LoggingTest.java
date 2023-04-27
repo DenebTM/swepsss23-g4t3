5ea6880ddadf6b4c4440929eccde1fc337d519e9
Luana Bach Gadelha <luana.bach-gadelha@student.uibk.ac.at>
2023-04-27 18:03:14 +0200

    added test for db logging

--- src/test/java/at/qe/skeleton/tests/LoggingTest.java
+++ src/test/java/at/qe/skeleton/tests/LoggingTest.java
@@ -1,14 +1,18 @@
 package at.qe.skeleton.tests;
 
+import net.bytebuddy.asm.MemberSubstitution;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.io.File;
+import java.sql.*;
 
 @SpringBootTest
+@WebAppConfiguration
 public class LoggingTest {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingTest.class);
@@ -35,5 +39,21 @@ public class LoggingTest {
         File error = new File("/logs/warnError.log");
         Assertions.assertTrue(error.exists());
         Assertions.assertTrue(error.length() > 0);
+    }
+
+    @Test
+    void testDbLog() throws SQLException {
+        logger.info("Test info message");
+        ResultSet rs = null;
+        try {
+            String url = "jdbc:h2:mem:skel";
+            Connection con = DriverManager.getConnection(url, "sa", "password");
+
+            Statement stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT * from LOGGING_EVENT");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertNotNull(rs);
     }
 }
