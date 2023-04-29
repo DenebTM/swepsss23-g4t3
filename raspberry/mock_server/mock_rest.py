47e8d381286c1f54a953e2b8f2cb4570bbae91f4
REDACTED-2 <REDACTED-2@student.uibk.ac.at>
2023-04-29 15:22:57 +0200

    changes to quotation marks and added status if accesspoint at least advertised once

--- raspberry/mock_server/mock_rest.py
+++ raspberry/mock_server/mock_rest.py
@@ -2,73 +2,94 @@ from flask import Flask,jsonify
 import time 
 
 app = Flask(__name__)
-
-time_now = int(time.time())
-
+status_called = False
 #Route that initiates connection
 @app.route('/access-points/', methods=['POST'])
 def status():
+    global status_called
+    status_called = True
     response = {'name': 'office1', 'serverAddress': 'localhost'}
     return jsonify(response), 200
 
 #Route that polls for connection-update
 @app.route('/access-points/1/', methods=['GET'])
 def accesspoint_connection():
-    if time_now == time_now + 500:
-        response = {"status": 'offline'}
+    if status_called: 
+        if int(time.time()) >= time_now + 180:
+            response = {'status': 'offline'}
+        else:
+            response = {'status': 'searching'}
+        return jsonify(response), 200
     else:
-        response = {"status": 'searching'}
-    return jsonify(response), 200
+        return 401
 
 #Route that updates Sensorstation Thressholds
 @app.route('/access-points/1/sensor-stations/1/', methods=['GET'])
 def thresshold_update():
-    response = {
-            "name": "SensorStation1",
-            "transmissioninterval": 60,
-            "thresholds": {
-                "temperature_max": 25,
-                "temperature_min": 10,
-                "humidity_max": 80,
-                "humidity_min": 30,
-                "air_pressure_max": 1000,
-                "air_pressure_min": 900,
-                "illuminance_max": 1000,
-                "illuminance_min": 100,
-                "air_quality_index_max": 50,
-                "air_quality_index_min": 0,
-                "soil_moisture_max": 50,
-                "soil_moisture_min": 10
+    if status_called:
+        response = {
+                'name': 'SensorStation1',
+                'transmissioninterval': 60,
+                'thresholds': {
+                    'temperature_max': 25,
+                    'temperature_min': 10,
+                    'humidity_max': 80,
+                    'humidity_min': 30,
+                    'air_pressure_max': 1000,
+                    'air_pressure_min': 900,
+                    'illuminance_max': 1000,
+                    'illuminance_min': 100,
+                    'air_quality_index_max': 50,
+                    'air_quality_index_min': 0,
+                    'soil_moisture_max': 50,
+                    'soil_moisture_min': 10
+                    }
                 }
-            }
-    
-    return jsonify(response), 200
+        
+        return jsonify(response), 200
+    else:
+        return 401
 
 # Route that asks for Instructions for each Sensorstation
 @app.route('/access-points/1/sensor-stations/', methods=['GET'])
 def ask_for_instructions_ss():
-    response = [
-        {201: "OFFLINE"},
-        {202: "ONLINE"},
-        {103: "PAIRING"}
+    if status_called:
+        response = [
+            {201: 'OFFLINE'},
+            {202: 'ONLINE'},
+            {103: 'PAIRING'}
         ]
 
-    return jsonify(response), 200
+        return jsonify(response), 200
+    else:
+        return 401
 
 # Route to send sensor failures to backend
-@app.route("sensor-stations/101", methods=["PUT"])
+@app.route('sensor-stations/101', methods=['PUT'])
 def report_connection_to_ss_to_backend():
-    return 200
-
+    if status_called:
+        return 200
+    else:
+        return 401
+    
 # Route to send sensor failures to backend
-@app.route("/access-points/1/sensor-stations/101", methods=["PUT"])
+@app.route('/access-points/1/sensor-stations/101', methods=['PUT'])
 def send_sensor_failures():
-    return 200
+    if status_called:
+        return 200
+    else:
+        return 401
+    
 
 #Route to send sensor data
-@app.route("/access-points/1/sensor-stations/101", methods=["POST"])
+@app.route('/access-points/1/sensor-stations/101', methods=['POST'])
 def send_sensor_data():
-    return 200
+    if status_called:
+        return 200
+    else:
+        return 401
+    
 
 if __name__ == '__main__':
+    time_now = int(time.time())
     app.run()
