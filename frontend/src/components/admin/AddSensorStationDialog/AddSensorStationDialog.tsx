591eacf33209c326fa100374e30c35bc5cb4d501
Jamie Fox <james.fox@student.uibk.ac.at>
2023-05-02 14:43:38 +0200

    Add reusable DialogHeader component

--- frontend/src/components/admin/AddSensorStationDialog/AddSensorStationDialog.tsx
+++ frontend/src/components/admin/AddSensorStationDialog/AddSensorStationDialog.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 import Dialog from '@mui/material/Dialog'
 import DialogContent from '@mui/material/DialogContent'
 
+import { DialogHeader } from '@component-lib/DialogHeader'
 import { AccessPointId } from '~/models/accessPoint'
 import { theme } from '~/styles/theme'
+
+const dialogTitleId = 'add-sensorstation-dialog-title'
 
 interface AddSensorStationModalProps {
   accessPointId?: AccessPointId
@@ -22,14 +25,20 @@ export const AddSensorStationModal: React.FC<AddSensorStationModalProps> = (
     <Dialog
       open={props.open}
       onClose={props.onClose}
-      aria-labelledby="add-sensorstation-dialog-title"
+      aria-labelledby={dialogTitleId}
       PaperProps={{
         sx: { minWidth: '70%', padding: theme.spacing(1, 3, 2) },
       }}
     >
-      HEADER
-      <DialogContent sx={{ textAlign: 'center' }}>CONTENTS</DialogContent>
-      ACTIONS
+      <DialogHeader
+        handleClose={props.onClose}
+        subtitle="Pair with a new sensor station below"
+        titleId={dialogTitleId}
+        title="Add Greenhouse"
+      />
+      <DialogContent sx={{ textAlign: 'center' }}>
+        to add: dialog contents
+      </DialogContent>
     </Dialog>
   )
 }
