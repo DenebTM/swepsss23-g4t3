2da5d42c579049c5d1141fa751b598a986c30a25
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-05-02 15:22:12 +0200

    Add aggregation period edit functionality

--- frontend/src/components/greenhouses/myGreenhouses/GreenhouseAccordion/GreenhouseAccordionContents/EditableCell/AggregationPeriodEditableCell.tsx
+++ frontend/src/components/greenhouses/myGreenhouses/GreenhouseAccordion/GreenhouseAccordionContents/EditableCell/AggregationPeriodEditableCell.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
+import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 
+import { onEnterKeypress } from '~/common'
 import { SensorStation } from '~/models/sensorStation'
 
 import { EditableCellProps } from '../EditableTableRow'
@@ -9,6 +11,8 @@ import { EditableCellProps } from '../EditableTableRow'
 interface AggregationPeriodEditableCellProps extends EditableCellProps<number> {
   /** aria label of row title field */
   labelledBy: string
+  /** Minimum supported aggregation period value (in seconds) */
+  minAggregationPeriod: number
   /** The sensor station this cell corresponds to */
   sensorStation: SensorStation
 }
@@ -20,13 +24,30 @@ interface AggregationPeriodEditableCellProps extends EditableCellProps<number> {
 export const AggregationPeriodEditableCell: React.FC<
   AggregationPeriodEditableCellProps
 > = (props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const isInvalidValue = props.rowValue < props.minAggregationPeriod
+
   return props.editing ? (
-    <Typography {...props.typographyProps}>
-      {props.sensorStation.aggregationPeriod} seconds
-    </Typography>
+    <TextField
+      required
+      label="Aggregation period"
+      type="number"
+      inputRef={inputRef}
+      onKeyPress={onEnterKeypress(props.saveRow)}
+      value={props.rowValue}
+      fullWidth
+      autoFocus
+      onChange={(e) => props.setRowValue(Number(e.target.value))}
+      error={isInvalidValue}
+      sx={{ maxWidth: '12em' }}
+      helperText={
+        isInvalidValue
+          ? `Please enter a value of at least ${props.minAggregationPeriod} seconds`
+          : ''
+      }
+    />
   ) : (
-    <Typography {...props.typographyProps}>
-      {props.sensorStation.aggregationPeriod} seconds
-    </Typography>
+    <Typography {...props.typographyProps}>{props.rowValue} seconds</Typography>
   )
 }
