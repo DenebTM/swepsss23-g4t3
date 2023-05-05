95f3cd22189a1b16b1194ec1a26b599005ac582b
Jamie Fox <james.fox@student.uibk.ac.at>
2023-05-05 15:04:39 +0200

    Show current uploading status in upload button

--- frontend/src/components/photoUpload/PhotoUploadBox/UploadArea.tsx
+++ frontend/src/components/photoUpload/PhotoUploadBox/UploadArea.tsx
@@ -28,10 +28,13 @@ export enum UploadStatus {
 
 /** Higher-order component to create a clickable upload area */
 export const getUploadArea = (
+  uploadStatus: UploadStatus,
   setUploadStatus: Dispatch<SetStateAction<UploadStatus>>
 ): React.FC<UploadButtonProps> =>
   asUploadButton((props: UploadButtonProps) => {
-    const [uploading, setUploading] = useState(false)
+    const [uploading, setUploading] = useState(
+      uploadStatus === UploadStatus.UPLOADING
+    )
 
     const addSnackbarMessage = useAddSnackbarMessage()
 
@@ -53,6 +56,7 @@ export const getUploadArea = (
         body: item.uploadResponse.data ?? 'Unable to process photo',
         type: MessageType.ERROR,
       })
+      setUploading(false)
       setUploadStatus(UploadStatus.FAILED)
     })
 
