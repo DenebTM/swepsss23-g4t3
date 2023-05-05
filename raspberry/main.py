8270fa87ce322acdea894f79ad358f2ff7284ff6
csaw5791 <Andreas.Schett@student.uibk.ac.at>
2023-05-05 18:11:50 +0200

    Status in ap statuts changed to searching again

--- raspberry/main.py
+++ raspberry/main.py
@@ -105,7 +105,7 @@ async def polling_loop(connection_request, session):
         print('this is inside the ap loop and the status is ' + status)
         if status == 'OFFLINE':
             connection_request.set_result('Done')
-        elif status in ['ONLINE', 'PAIRING']:
+        elif status in ['ONLINE', 'SEARCHING']:
             sensorstations = await search_for_sensorstations()
             await send_sensorstations_to_backend(session, sensorstations)
         await asyncio.sleep(10)
