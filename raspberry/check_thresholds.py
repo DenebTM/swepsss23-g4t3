1d49e197e2cf922f62ca73d7bcd110f9fee1f676
REDACTED-2 <REDACTED-2@student.uibk.ac.at>
2023-05-06 11:30:06 +0200

    small changes and addition of sensor_active_uuid

--- raspberry/check_thresholds.py
+++ raspberry/check_thresholds.py
@@ -1,10 +1,11 @@
 import common
 from database_operations import get_sensor_data_thresholds, get_sensor_data_averages
 import asyncio
+import json
 
 #TODO: deneb fragen wo die ID gspeichert is
 
-async def check_values_for_thresholds(sensorstation_client, sensorstation_id, transmission_interval):
+async def check_values_for_thresholds(sensorstation_client, sensorstation_id, transmission_interval,session):
     await asyncio.sleep(transmission_interval)
     try:
         thresholds_dict = await get_sensor_data_thresholds(sensorstation_id)
@@ -13,27 +14,27 @@ async def check_values_for_thresholds(sensorstation_client, sensorstation_id, tr
             max_threshold = thresholds_dict[sensor+'_max']
             min_threshold = thresholds_dict[sensor+'_min']
             if not min_threshold <= average_value <= max_threshold:
-                await send_error_to_sensorstation(sensorstation_client, sensorstation_id)
-                await send_error_to_backend(sensorstation_client, sensorstation_id)
+                await send_error_to_sensorstation(sensorstation_client, sensorstation_id, sensor, session)
+                await send_error_to_backend(sensorstation_id, session)
     except Exception as e:
         print(e)
 
                         
-async def send_error_to_sensorstation(sensor_station_client, sensor):
+async def send_error_to_sensorstation(sensorstation_client, sensorstation_id, sensor, session):
     errorCode = 1
     errorCodeByteArray = errorCode.to_bytes(1, byteorder='little')
     try:
         sensor_uuid = common.failure_uuids[sensor]
-        await sensor_station_client.write_gatt_char(sensor_uuid, errorCodeByteArray)
+        await sensorstation_client.write_gatt_char(sensor_uuid, errorCodeByteArray)
     except:
          print('couldnt write to gatt')
     #TODO log error code
 
 
-async def send_error_to_backend(sensor_station_client, sensor):
-    try:
-        data = {'sensorstation': sensor_station_client.name, 'sensor': sensor}
-        #request.post('idk address of errors', json=data)
-    except:
-        print('couldnt connect to backend or smth')
-    
+async def send_error_to_backend(sensorstation_id, session):
+        data = {'id': sensorstation_id, 'status': 'WARNING'}
+        data = json.dumps(data)
+        # async with session.put('address for sensorstations_warning and clear', json=data) as response:
+        #     status = await response.status()
+        #     print(status)
+
