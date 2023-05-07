6c05bda52770c13a25429f8d657c1894ddab6cd4
Luana Bach Gadelha <luana.bach-gadelha@student.uibk.ac.at>
2023-05-07 19:21:25 +0200

    added several getLogs() methods

--- src/main/java/at/qe/skeleton/services/LoggingService.java
+++ src/main/java/at/qe/skeleton/services/LoggingService.java
@@ -5,7 +5,10 @@ import at.qe.skeleton.repositories.LoggingEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -20,6 +23,36 @@ public class LoggingService {
      */
     public List<LoggingEvent> getAllLogs() {
         return loggingEventRepository.findAll();
+    }
+
+    public List<LoggingEvent> getAllLogsTo(LocalDateTime to) {
+        ArrayList<LoggingEvent> result = new ArrayList<>();
+        List<LoggingEvent> logs = loggingEventRepository.findAllByOrderByTimestmpAsc();
+        for (LoggingEvent l :
+                logs) {
+            if (l.getTimestmp().isAfter(to)) {
+                break;
+            }
+            result.add(l);
+        }
+        return result;
+    }
+
+    public List<LoggingEvent> getAllLogsFrom(LocalDateTime from) {
+        ArrayList<LoggingEvent> result = new ArrayList<>();
+        List<LoggingEvent> logs = loggingEventRepository.findAllByOrderByTimestmpDesc();
+        for (LoggingEvent l :
+                logs) {
+            if (l.getTimestmp().isBefore(from)) {
+                break;
+            }
+            result.add(l);
+        }
+        return result;
+    }
+
+    public List<LoggingEvent> getAllLogsInTimeInterval(LocalDateTime from, LocalDateTime to) {
+        return loggingEventRepository.findAllByTimestmpGreaterThanAndTimestmpLessThanOrderByTimestmpAsc(from, to);
     }
 
     public List<LoggingEvent> loadLogsByTimestamp(LocalDateTime timestamp) {
