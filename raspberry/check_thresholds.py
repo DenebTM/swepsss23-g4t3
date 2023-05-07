02a882ff56e11e8eddf78b02fbf073f7756f4e80
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-05-07 19:04:06 +0200

    fix indentation etc

--- raspberry/check_thresholds.py
+++ raspberry/check_thresholds.py
@@ -1,7 +1,6 @@
 import common
 from database_operations import get_sensor_data_thresholds, get_sensor_data_averages
 import asyncio
-import json
 
 async def check_values_for_thresholds(sensorstation_client, sensorstation_id, transmission_interval,session):
     await asyncio.sleep(transmission_interval)
@@ -12,13 +11,14 @@ async def check_values_for_thresholds(sensorstation_client, sensorstation_id, tr
             max_threshold = thresholds_dict[sensor+'_max']
             min_threshold = thresholds_dict[sensor+'_min']
             if not min_threshold <= average_value <= max_threshold:
-                await send_error_to_sensorstation(sensorstation_client, sensorstation_id, sensor, session)
-                await send_error_to_backend(sensorstation_id, session)
+                await send_warning_to_sensorstation(sensorstation_client, sensorstation_id, sensor, session)
+                await send_warning_to_backend(sensorstation_id, session)
     except Exception as e:
         print(e)
+        #TODO log error code
 
                         
-async def send_error_to_sensorstation(sensorstation_client, sensorstation_id, sensor, session):
+async def send_warning_to_sensorstation(sensorstation_client, sensorstation_id, sensor, session):
     errorCode = 1
     errorCodeByteArray = errorCode.to_bytes(1, byteorder='little')
     try:
@@ -27,24 +27,23 @@ async def send_error_to_sensorstation(sensorstation_client, sensorstation_id, se
         await sensorstation_client.start_notify(
             common.warning_active_uuid,
             lambda char, data: asyncio.create_task(
-            clear_warning_on_backend(sensorstation_id, session, data))   
+                clear_warning_on_backend(sensorstation_id, session, data)
+            )
         )
     except:
-         print('couldnt write to gatt')
-    #TODO log error code
+        print('couldnt write to gatt')
+        #TODO log error code
 
 
-async def send_error_to_backend(sensorstation_id, session):
-        data = {'id': sensorstation_id, 'status': 'WARNING'}
-        data = json.dumps(data)
-        async with session.put(common.web_server_address + '/sensor-stations/' + str(sensorstation_id), json=data) as response:
-               print(response.status)
-        #TODO: Log communication
+async def send_warning_to_backend(sensorstation_id, session):
+    data = {'id': sensorstation_id, 'status': 'WARNING'}
+    async with session.put(common.web_server_address + '/sensor-stations/' + str(sensorstation_id), json=data) as response:
+        print(response.status)
+    #TODO: Log communication
 
 async def clear_warning_on_backend(sensorstation_id, session, data):
     if int.from_bytes(data, 'little', signed=False) == 0:
         data = {'id': sensorstation_id, 'status': 'OK'}
-        data = json.dumps(data)
         async with session.put(common.web_server_address + '/sensor-stations/' + str(sensorstation_id), json=data) as response:
-               print(response.status)
+            print(response.status)
         #TODO: Log communication
