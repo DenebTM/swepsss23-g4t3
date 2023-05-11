674ddb025c5619f3365457ebfe61943f489fda8f
REDACTED-2 <REDACTED-2@student.uibk.ac.at>
2023-05-11 10:16:28 +0200

    Modify test_sensorvalues to not sleep and only run once

--- raspberry/tests/test_sensorvalues_operations.py
+++ raspberry/tests/test_sensorvalues_operations.py
@@ -1,6 +1,6 @@
 import unittest
 
-from unittest.mock import AsyncMock, patch
+from unittest.mock import MagicMock, AsyncMock, patch
 from bleak import BleakError
 
 
@@ -10,15 +10,22 @@ from database_operations import save_sensor_values_to_database
 
 class TestReadSensorValue(unittest.IsolatedAsyncioTestCase):
 
+    @patch('asyncio.sleep', return_value=None)
     @patch('database_operations.save_sensor_values_to_database')
-    #@patch('sensorvalues_operations.BleakClient')
-    async def test_read_sensorvalues(self, save_sensor_values_to_database):
+    async def test_read_sensorvalues(self, save_sensor_values_to_database, sleep_mock):
+
+
+        connection_request = MagicMock()
         BleakClient = AsyncMock()
+
+        connection_request.done.side_effect = [False, True]
+        BleakClient.is_connected.return_value = True
+
         #mock reading characteristic of BleakClient
         BleakClient.read_gatt_char.return_value=int(10).to_bytes(2,byteorder='little')
 
         # call function with mock client
-        await read_sensorvalues(BleakClient, 1)
+        await read_sensorvalues(BleakClient, 1, connection_request)
 
         # assert that saveSensorValuesToDatabase was called once with the expected arguments
         save_sensor_values_to_database.assert_called_once_with(1, 10, 10, 10, 10, 10, 10)
