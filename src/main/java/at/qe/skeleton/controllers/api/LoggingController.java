111dd6e91ee367b49c56fe32b587a7d3aa0abad1
DenebTM <deneb@screee.ee>
2023-05-12 23:45:16 +0200

    use exceptions to signal errors

--- src/main/java/at/qe/skeleton/controllers/api/LoggingController.java
+++ src/main/java/at/qe/skeleton/controllers/api/LoggingController.java
@@ -1,21 +1,15 @@
 package at.qe.skeleton.controllers.api;
 
+import at.qe.skeleton.controllers.errors.BadRequestException;
 import at.qe.skeleton.models.LoggingEvent;
 import at.qe.skeleton.services.LoggingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.DateTimeException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -37,27 +31,27 @@ public class LoggingController implements BaseRestController{
      */
     @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/logs")
-    public ResponseEntity<Object> getLogs(@RequestBody Map<String, Object> json) {
+    public ResponseEntity<List<LoggingEvent>> getLogs(@RequestBody Map<String, Object> json) {
         List<LoggingEvent> logs;
         if (json.containsKey("from")) {
             if (json.containsKey("to")) {
                 try {
                     logs = loggingService.getAllLogsInTimeInterval(json.get("from"), json.get("to"));
                 } catch (Exception e) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parameters have wrong format (must be 'yyyy-MM-dd')");
+                    throw new BadRequestException("Parameters have wrong format (must be 'yyyy-MM-dd')");
                 }
             } else {
                 try {
                     logs = loggingService.getAllLogsFrom(json.get("from"));
                 } catch (Exception e) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parameter 'from' has wrong format (must be 'yyyy-MM-dd')");
+                    throw new BadRequestException("Parameter 'from' has wrong format (must be 'yyyy-MM-dd')");
                 }
             }
         } else if (json.containsKey("to")) {
             try {
                 logs = loggingService.getAllLogsTo(json.get("to"));
             } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parameter 'to' has wrong format (must be 'yyyy-MM-dd')");
+                throw new BadRequestException("Parameter 'to' has wrong format (must be 'yyyy-MM-dd')");
             }
         } else {
             logs = loggingService.getAllLogs();
@@ -66,7 +60,7 @@ public class LoggingController implements BaseRestController{
             try {
                 logs = loggingService.filterLogsByLevel(logs, json.get("level"));
             } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parameter 'level' has wrong format (must be a String of either 'INFO', 'WARN', or 'ERROR'");
+                throw new BadRequestException("Parameter 'level' has wrong format (must be a String of either 'INFO', 'WARN', or 'ERROR'");
             }
         }
         return ResponseEntity.ok(logs);
