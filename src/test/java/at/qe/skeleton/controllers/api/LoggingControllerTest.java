d7d580f04da7aec854969add006bb7afe2e2a511
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-05-13 00:02:08 +0200

    refactor and fix LoggingControllerTest

--- src/test/java/at/qe/skeleton/controllers/api/LoggingControllerTest.java
+++ src/test/java/at/qe/skeleton/controllers/api/LoggingControllerTest.java
@@ -1,13 +1,8 @@
 package at.qe.skeleton.controllers.api;
 
 import at.qe.skeleton.models.LoggingEvent;
-import at.qe.skeleton.repositories.LoggingEventRepository;
 import at.qe.skeleton.services.LoggingService;
-import at.qe.skeleton.tests.LoggingTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatusCode;
@@ -16,15 +11,9 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @WebAppConfiguration
@@ -34,22 +23,19 @@ public class LoggingControllerTest {
     private LoggingController loggingController;
     @Autowired
     private LoggingService loggingService;
-    @Autowired
-    private LoggingEventRepository loggingEventRepository;
 
     @DirtiesContext
     @Test
     @WithMockUser(username = "admin", authorities = {"ADMIN"})
     public void testGetAllLogs() {
-        ResponseEntity logs = loggingController.getLogs(new HashMap<>());
         int numberOfLogs = loggingService.getAllLogs().size();
-        Assertions.assertEquals(HttpStatusCode.valueOf(200), logs.getStatusCode());
-        try {
-            Assertions.assertEquals(numberOfLogs, ((Collection) logs.getBody()).size());
-            Assertions.assertTrue(((Collection) logs.getBody()).size() > 0);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+
+        var response = loggingController.getLogs(new HashMap<>());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+        var logs = response.getBody();
+        assertNotNull(logs);
+        assertEquals(numberOfLogs, logs.size());
     }
 
     @DirtiesContext
@@ -57,58 +43,53 @@ public class LoggingControllerTest {
     @WithMockUser(username = "admin", authorities = {"ADMIN"})
     void testGetLogsFrom(){
         Map<String, Object> json = new HashMap<>();
-        json.put("from", "2023-05-10");
-        Object from = "2023-05-10";
+        String from = "2023-05-10";
+        json.put("from", from);
 
-        ResponseEntity logs = loggingController.getLogs(json);
+        var response = loggingController.getLogs(json);
         int numberOfLogs = loggingService.getAllLogsFrom(from).size();
-        Assertions.assertEquals(HttpStatusCode.valueOf(200), logs.getStatusCode());
-        try {
-            Assertions.assertEquals(numberOfLogs, ((Collection) logs.getBody()).size());
-            Assertions.assertTrue(((Collection) logs.getBody()).size() > 0);
-            Assertions.assertTrue(loggingService.getAllLogs().size() > ((Collection) logs.getBody()).size());
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+        var logs = response.getBody();
+        assertNotNull(logs);
+        assertEquals(numberOfLogs, logs.size());
+        assertTrue(loggingService.getAllLogs().size() >= logs.size());
     }
 
     @Test
     @WithMockUser(username = "admin", authorities = {"ADMIN"})
     void testGetLogsTo() {
         Map<String, Object> json = new HashMap<>();
-        json.put("to", "2023-05-09");
-        Object to = "2023-05-09";
-        ResponseEntity logs = loggingController.getLogs(json);
+        String to = "2023-05-09";
+        json.put("to", to);
+
+        var response = loggingController.getLogs(json);
         int numberOfLogs = loggingService.getAllLogsTo(to).size();
-        Assertions.assertEquals(HttpStatusCode.valueOf(200), logs.getStatusCode());
-        try {
-            Assertions.assertEquals(numberOfLogs, ((Collection) logs.getBody()).size());
-            Assertions.assertTrue(((Collection) logs.getBody()).size() > 0);
-            Assertions.assertTrue(loggingService.getAllLogs().size() > ((Collection) logs.getBody()).size());
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+        var logs = response.getBody();
+        assertNotNull(logs);
+        assertEquals(numberOfLogs, logs.size());
+        assertTrue(loggingService.getAllLogs().size() >= logs.size());
     }
 
     @Test
     @WithMockUser(username = "admin", authorities = {"ADMIN"})
     void testGetLogsInTimeInterval() {
         Map<String, Object> json = new HashMap<>();
-        json.put("from", "2023-05-09");
-        json.put("to", "2023-05-11");
-        Object from = "2023-05-09";
-        Object to = "2023-05-11";
-        ResponseEntity logs = loggingController.getLogs(json);
-        System.out.println();
+        String from = "2023-05-09";
+        String to = "2023-05-11";
+        json.put("from", from);
+        json.put("to", to);
+
+        var response = loggingController.getLogs(json);
         int numberOfLogs = loggingService.getAllLogsInTimeInterval(from, to).size();
-        Assertions.assertEquals(HttpStatusCode.valueOf(200), logs.getStatusCode());
-        try {
-            Assertions.assertEquals(numberOfLogs, ((Collection) logs.getBody()).size());
-            Assertions.assertTrue(((Collection) logs.getBody()).size() > 0);
-            Assertions.assertTrue(loggingService.getAllLogs().size() > ((Collection) logs.getBody()).size());
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+        var logs = response.getBody();
+        assertNotNull(logs);
+        assertEquals(numberOfLogs, logs.size());
+        assertTrue(loggingService.getAllLogs().size() > logs.size());
     }
 
     @Test
@@ -116,20 +97,23 @@ public class LoggingControllerTest {
     void testGetLogsByLevel() {
         Map<String, Object> json = new HashMap<>();
         List<String> levels = List.of("INFO", "WARN", "ERROR");
-        for (String s :
-                levels) {
+        
+        int totalFilteredLogCount = 0;
+        for (String s : levels) {
             json.put("level", s);
-            ResponseEntity controllerLogs = loggingController.getLogs(json);
-            List<LoggingEvent> serviceLogs = loggingService.getAllLogs();
-            loggingService.filterLogsByLevel(serviceLogs, s);
+            var serviceLogs = loggingService.getAllLogs();
+            serviceLogs = loggingService.filterLogsByLevel(serviceLogs, s);
+
+            var response = loggingController.getLogs(json);
             int numberOfLogs = serviceLogs.size();
-            Assertions.assertEquals(HttpStatusCode.valueOf(200), controllerLogs.getStatusCode());
-            try {
-                Assertions.assertEquals(numberOfLogs, ((Collection) controllerLogs.getBody()).size());
-                Assertions.assertTrue(((Collection) controllerLogs.getBody()).size() > 0);
-            } catch (NullPointerException e) {
-                e.printStackTrace();
-            }
+            assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+
+            var logs = response.getBody();
+            assertNotNull(logs);
+            assertEquals(numberOfLogs, logs.size());
+            totalFilteredLogCount += logs.size();
         }
+
+        assertTrue(totalFilteredLogCount <= loggingService.getAllLogs().size());
     }
 }
