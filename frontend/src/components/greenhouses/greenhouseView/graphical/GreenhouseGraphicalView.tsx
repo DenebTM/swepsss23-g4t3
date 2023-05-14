6a1d3a1868e49c5f168cae2061de9e395f5ed4d5
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-05-14 17:41:55 +0200

    Define donut height in one place

--- frontend/src/components/greenhouses/greenhouseView/graphical/GreenhouseGraphicalView.tsx
+++ frontend/src/components/greenhouses/greenhouseView/graphical/GreenhouseGraphicalView.tsx
@@ -2,6 +2,7 @@ import { cancelable } from 'cancelable-promise'
 import React, { useEffect, useState } from 'react'
 
 import Grid from '@mui/material/Unstable_Grid2'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 import { DashboardCard } from '@component-lib/DashboardCard'
 import dayjs from 'dayjs'
@@ -11,6 +12,7 @@ import { useSensorStations } from '~/hooks/appContext'
 import { useAddSnackbarMessage } from '~/hooks/snackbar'
 import { Measurement } from '~/models/measurement'
 import { SensorStation, SensorStationUuid } from '~/models/sensorStation'
+import { theme } from '~/styles/theme'
 
 import { GreenhouseAirMetrics } from './GreenhouseDonuts/GreenhouseAirMetrics'
 import { GreenhouseMetricDonuts } from './GreenhouseDonuts/GreenhouseMetricDonuts'
@@ -28,6 +30,10 @@ export const GreenhouseGraphicalView: React.FC<GreenhouseGraphicalViewProps> = (
 ) => {
   const sensorStations = useSensorStations()
   const addSnackbarMessage = useAddSnackbarMessage()
+  const breakMd = useMediaQuery(theme.breakpoints.down('md'))
+
+  /** Donut height in px. qqjf TODO make this responsive  */
+  const donutHeight = breakMd ? 250 : 175
 
   const [sensorStation, setSensorStation] = useState<SensorStation | null>(null)
   const [measurements, setMeasurements] = useState<Measurement[]>()
@@ -85,6 +91,7 @@ export const GreenhouseGraphicalView: React.FC<GreenhouseGraphicalViewProps> = (
           <Grid xs={12} md={8}>
             <DashboardCard>
               <GreenhouseMetricDonuts
+                donutHeight={donutHeight}
                 measurement={measurements.length > 0 ? measurements[0] : null}
                 sensorStation={sensorStation}
               />
@@ -93,6 +100,7 @@ export const GreenhouseGraphicalView: React.FC<GreenhouseGraphicalViewProps> = (
           <Grid xs={12} md={4}>
             <DashboardCard>
               <GreenhouseAirMetrics
+                donutHeight={donutHeight}
                 measurement={measurements.length > 0 ? measurements[0] : null}
                 sensorStation={sensorStation}
               />
