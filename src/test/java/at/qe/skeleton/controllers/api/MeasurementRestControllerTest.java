7b24331dbf90d81371a86358a9f015ed34dd774b
DenebTM <deneb@screee.ee>
2023-05-15 01:53:43 +0200

    Change behaviour of from/to in MeasurementRestController, Uncomment test

--- src/test/java/at/qe/skeleton/controllers/api/MeasurementRestControllerTest.java
+++ src/test/java/at/qe/skeleton/controllers/api/MeasurementRestControllerTest.java
@@ -10,10 +10,10 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.time.Instant;
-// import java.time.LocalDate;
-// import java.time.ZoneId;
-// import java.time.ZonedDateTime;
-// import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,13 +25,13 @@ class MeasurementRestControllerTest {
 
     Integer testSsId = 1;
 
-    // private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-    // private static Instant parseInstant(String charseq) {
-    //     LocalDate date = LocalDate.parse(charseq, formatter);
-    //     ZonedDateTime zoneDate = ZonedDateTime.of(date.atTime(0, 0, 0), ZoneId.systemDefault());
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+    private static Instant parseInstant(String charseq) {
+        LocalDate date = LocalDate.parse(charseq, formatter);
+        ZonedDateTime zoneDate = ZonedDateTime.of(date.atTime(0, 0, 0), ZoneId.systemDefault());
 
-    //     return zoneDate.toInstant();
-    // }
+        return zoneDate.toInstant();
+    }
 
     @Autowired
     private MeasurementRestController mmRestController;
@@ -45,20 +45,20 @@ class MeasurementRestControllerTest {
      * see: https://discourse.hibernate.org/t/force-initializing-collection-loading/7172
      */
 
-    // @Test
-    // void testGetAllMeasurementsInTimeRange() {
-    //     Instant from = parseInstant("2023-03-01T20:10:40Z");
-    //     Instant to = parseInstant("2023-05-20T10:40:00Z");
+    @Test
+    void testGetAllMeasurementsInTimeRange() {
+        Instant from = parseInstant("2023-03-01T20:10:40Z");
+        Instant to = parseInstant("2023-05-20T10:40:00Z");
 
-    //     int measurementCount = measurementService.getMeasurements(testSsId, from, to).size();
+        int measurementCount = measurementService.getMeasurements(testSsId, from, to).size();
 
-    //     var response = mmRestController.getMeasurementsBySS(testSsId, from, to);
-    //     assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        var response = mmRestController.getMeasurementsBySS(testSsId, from, to);
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
 
-    //     var measurements = response.getBody();
-    //     assertNotNull(measurements);
-    //     assertEquals(measurementCount, measurements.size());
-    // }
+        var measurements = response.getBody();
+        assertNotNull(measurements);
+        assertEquals(measurementCount, measurements.size());
+    }
 
     @Test
     void testGetAllCurrentMeasurements() {
