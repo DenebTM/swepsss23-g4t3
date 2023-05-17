3d2f0c318233afac8996cc5b097ef0c2fc7e7cce
DenebTM <deneb@screee.ee>
2023-05-17 04:20:24 +0200

    working interceptors for logback

--- src/main/java/at/qe/skeleton/configs/logging/MDCAbstractEntityFilter.java
+++ src/main/java/at/qe/skeleton/configs/logging/MDCAbstractEntityFilter.java
@@ -4,22 +4,21 @@ import java.io.IOException;
 
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 import at.qe.skeleton.models.enums.LogEntityType;
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public abstract class MDCAbstractEntityFilter extends OncePerRequestFilter {
+public abstract class MDCAbstractEntityFilter implements HandlerInterceptor {
 
     protected abstract LogEntityType entityType();
     protected abstract Object entityId(HttpServletRequest request);
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws ServletException, IOException {
         final var entityType = entityType();
         final var entityId = entityId(request); 
@@ -27,7 +26,7 @@ public abstract class MDCAbstractEntityFilter extends OncePerRequestFilter {
             MDC.put(entityType.name(), entityId.toString());
         }
 
-        filterChain.doFilter(request, response);   
+        return true;
     }
 
 }
