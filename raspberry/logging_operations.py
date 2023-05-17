e9ec612280ed23f02a3ef4a19b5a79c6f728e34b
REDACTED-2 <REDACTED-2@student.uibk.ac.at>
2023-05-17 10:06:19 +0200

    Finish logging basic config

--- raspberry/logging_operations.py
+++ raspberry/logging_operations.py
@@ -1,6 +1,6 @@
 import logging
 
-formatter = logging.basicConfig(format='%(asctime)s-%(levelname)s-%(message)s')
+logging.basicConfig(filename='temp.log', encoding='utf-8', format='%(asctime)s-%(levelname)s-%(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
 
 async def append_temporary_to_permanent_logfile():
     with open('temp.log', 'r') as temp_file:
