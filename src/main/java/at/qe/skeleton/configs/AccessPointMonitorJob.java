2f7231cd52bcb2e5fb2701019f4247f1ee3d71e1
DenebTM <deneb@screee.ee>
2023-05-18 14:55:34 +0200

    log access point operations

--- src/main/java/at/qe/skeleton/configs/AccessPointMonitorJob.java
+++ src/main/java/at/qe/skeleton/configs/AccessPointMonitorJob.java
@@ -10,9 +10,11 @@ import org.springframework.scheduling.annotation.Scheduled;
 import at.qe.skeleton.models.AccessPoint;
 import at.qe.skeleton.models.SensorStation;
 import at.qe.skeleton.models.enums.AccessPointStatus;
+import at.qe.skeleton.models.enums.LogEntityType;
 import at.qe.skeleton.models.enums.SensorStationStatus;
 import at.qe.skeleton.repositories.AccessPointRepository;
 import at.qe.skeleton.repositories.SensorStationRepository;
+import at.qe.skeleton.services.LoggingService;
 
 @Configuration
 @EnableScheduling
@@ -27,15 +29,25 @@ public class AccessPointMonitorJob {
     @Autowired
     private SensorStationRepository ssRepository;
 
+    @Autowired
+    private LoggingService logger;
+
     @Scheduled(initialDelay = CHECK_INTERVAL, fixedDelay = CHECK_INTERVAL)
     public void checkAccessPoints() {
         for (AccessPoint ap : apRepository.findAll()) {
             if (ap.getLastUpdate().plusSeconds(AP_TIMEOUT).isBefore(Instant.now())) {
                 for (SensorStation ss : ap.getSensorStations()) {
+                    if (!ss.getStatus().equals(SensorStationStatus.OFFLINE)) {
+                        logger.info("Sensor station status changed to OFFLINE", LogEntityType.SENSOR_STATION, ss.getSsID(), getClass());
+                    }
+
                     ss.setStatus(SensorStationStatus.OFFLINE);
                     ssRepository.save(ss);
                 }
 
+                if (!ap.getStatus().equals(AccessPointStatus.OFFLINE)) {
+                    logger.info("Access point status changed to OFFLINE", LogEntityType.ACCESS_POINT, ap.getName(), getClass());
+                }
                 ap.setStatus(AccessPointStatus.OFFLINE);
                 apRepository.save(ap);
             }
