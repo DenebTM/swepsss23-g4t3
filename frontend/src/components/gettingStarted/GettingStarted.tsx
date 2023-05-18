e53a8839748266d5186057ea0b79f1901e852475
DenebTM <deneb@screee.ee>
2023-05-18 23:16:38 +0200

    Add section about pairing a greenhouse

--- frontend/src/components/gettingStarted/GettingStarted.tsx
+++ frontend/src/components/gettingStarted/GettingStarted.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import CheckIcon from '@mui/icons-material/Check'
 import Typography from '@mui/material/Typography'
 
+import { GreenhouseIcon } from '~/common'
 import { PAGE_URL } from '~/common'
 import { PageHeader } from '~/components/page/PageHeader'
 import { PageWrapper } from '~/components/page/PageWrapper'
@@ -30,7 +31,7 @@ export const GettingStarted: React.FC = () => {
       </Typography>
       <ol>
         <li>
-          Plug in the Raspberry Pi and connect it to the network via Wi-Fi
+          Plug in the Raspberry Pi and connect it to the network via Wi-Fi.
         </li>
         <li>
           Run the './configure' script to set up the correct web server address.
@@ -40,9 +41,41 @@ export const GettingStarted: React.FC = () => {
         <li>
           Run 'python3 main.py'. The Access Point will automatically register
           itself with the web server and be displayed on the{' '}
-          <Link to="/admin/access-points">Access Points</Link> page with a
-          yellow highlight and status "unconfirmed". Press the <CheckIcon />{' '}
-          button to confirm and allow it to communicate with the web server.
+          <Link to={PAGE_URL.manageAccessPoints.href}>
+            {PAGE_URL.manageAccessPoints.pageTitle}
+          </Link>{' '}
+          page with a yellow highlight and status "unconfirmed". Press the{' '}
+          <CheckIcon /> button to confirm and allow it to communicate with the
+          web server.
+        </li>
+      </ol>
+
+      <Typography variant="h6" fontStyle="italic">
+        Pairing a Greenhouse
+      </Typography>
+      <ol>
+        <li>Set the Greenhouse's ID using the DIP switch.</li>
+        <li>
+          Press the righmost button (the one connected to ) connected to pin D2
+          of the Arduino. The LED will begin flashing blue.
+        </li>
+        <li>
+          Navigate to{' '}
+          <Link to={PAGE_URL.manageAccessPoints.href}>
+            {PAGE_URL.manageAccessPoints.pageTitle}
+          </Link>
+        </li>
+        <li>
+          Click the <GreenhouseIcon /> button on the right.
+        </li>
+        <li>
+          Wait roughly 10 seconds, then open the Sensor Station dropdown menu.
+          Select the ID of the Greenhouse with the ID that you want to pair.
+        </li>
+        <li>
+          Press Confirm. After a few more seconds, the LED on the Greenhouse
+          should turn green, and you will be able to view measurements on the
+          corresponding page under "Dashboard".
         </li>
       </ol>
     </PageWrapper>
