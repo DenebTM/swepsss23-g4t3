8587fba4b36b0e983d19934a1d870a70a35e8b77
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-05-18 20:30:52 +0200

    Add simple recent activity list display

--- frontend/src/components/dashboard/RecentActivity/RecentActivity.tsx
+++ frontend/src/components/dashboard/RecentActivity/RecentActivity.tsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 
 import { DashboardCard } from '@component-lib/DashboardCard'
+import dayjs from 'dayjs'
+import { getLogs } from '~/api/endpoints/logs'
+import { useAddErrorSnackbar } from '~/hooks/snackbar'
+import { LogEntry } from '~/models/log'
+
+import { RecentActivityList } from './RecentActivityList'
 
 /**
  * Component to display recent activity in the dashboard
  */
 export const RecentActivity: React.FC = () => {
+  const addErrorSnackbar = useAddErrorSnackbar()
+  const [logEntries, setLogEntries] = useState<LogEntry[]>()
+
+  /** Fetch logs from the last week on component mount */
+  useEffect(() => {
+    getLogs({
+      from: dayjs().subtract(1, 'week').toISOString(),
+      to: dayjs().toISOString(),
+    })
+      .then((data) => {
+        setLogEntries(data)
+      })
+      .catch((err: Error) => {
+        addErrorSnackbar(err, 'Unable to fetch logs')
+      })
+  }, [])
+
   return (
-    <DashboardCard>
-      <div>Recent activity</div>
+    <DashboardCard
+      loading={typeof logEntries === 'undefined'}
+      empty={typeof logEntries !== 'undefined' && logEntries.length === 0}
+      emptyText="Recent notifications will appear here"
+    >
+      <Stack sx={{ width: '100%' }}>
+        <Typography color="onSurfaceVariant" variant="titleSmall">
+          Recent activity
+        </Typography>
+        {logEntries && <RecentActivityList logEntries={logEntries} />}
+      </Stack>
     </DashboardCard>
   )
 }
