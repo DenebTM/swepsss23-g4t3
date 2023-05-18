515ab74fd7cad1d44d838ffcddb5abd0666bbb92
DenebTM <deneb@screee.ee>
2023-05-19 01:17:57 +0200

    filter AVAILABLE sensorstations everywhere

--- frontend/src/hooks/appContext.ts
+++ frontend/src/hooks/appContext.ts
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { getSensorStations } from '~/api/endpoints/sensorStations/sensorStations'
 import { AppContext } from '~/contexts/AppContext/AppContext'
-import { SensorStation } from '~/models/sensorStation'
+import { SensorStation, StationStatus } from '~/models/sensorStation'
 
 import { useAddErrorSnackbar } from './snackbar'
 
@@ -10,7 +10,9 @@ import { useAddErrorSnackbar } from './snackbar'
  * Hook to get the current values of the fetched sensor stations in the global AppContext.
  * If the values have not been set yet, then fetches and saves these.
  */
-export const useSensorStations = (): SensorStation[] | null => {
+export const useSensorStations = (
+  onlyPaired?: boolean
+): SensorStation[] | null => {
   const { appState } = React.useContext(AppContext)
   const sensorStations: SensorStation[] | null = appState.sensorStations.data
   const loadSensorStations = useLoadSensorStations()
@@ -19,7 +21,11 @@ export const useSensorStations = (): SensorStation[] | null => {
   if (sensorStations === null) {
     loadSensorStations()
   }
-  return sensorStations
+
+  return onlyPaired
+    ? sensorStations?.filter((s) => s.status !== StationStatus.AVAILABLE) ??
+        null
+    : sensorStations
 }
 
 /**
