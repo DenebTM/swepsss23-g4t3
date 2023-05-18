42277e18aa7ad35198cbcd3037d4530370da8bfb
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-05-19 00:20:13 +0200

    Improve page setup disclaimer

--- frontend/src/components/gettingStarted/GettingStarted.tsx
+++ frontend/src/components/gettingStarted/GettingStarted.tsx
@@ -11,6 +11,7 @@ import { GreenhouseIcon } from '~/common'
 import { PAGE_URL } from '~/common'
 import { PageHeader } from '~/components/page/PageHeader'
 import { PageWrapper } from '~/components/page/PageWrapper'
+import { theme } from '~/styles/theme'
 
 import { PageTitle } from '../page/PageTitle'
 
@@ -32,23 +33,23 @@ export const GettingStarted: React.FC = () => {
         left={<PageTitle>{PAGE_URL.gettingStarted.pageTitle}</PageTitle>}
       />
 
-      <Stack spacing={2}>
-        <p>
-          <b>Note:</b> It is assumed that all devices are already set up with
-          the necessary dependencies/project files/firmware/wiring/etc.
-          <br />
-          <br />
-          For additional information, please refer to the project{' '}
-          <Link href="https://git.uibk.ac.at/informatik/qe/swess23/group4/g4t3/-/wikis/home">
-            wiki
-          </Link>{' '}
-          and{' '}
-          <Link href="https://git.uibk.ac.at/informatik/qe/swess23/group4/g4t3/-/blob/690e66b84beca72c807af26bdeec6f2d37fd7929/README.md">
-            README
-          </Link>
-          .
-        </p>
+      <Typography color="outlineVariant" variant="bodySmall" gutterBottom>
+        <b>Note:</b> this guide assumes that all devices are already set up with
+        the necessary dependencies/project files/firmware/wiring/etc.
+      </Typography>
+      <Typography color="outlineVariant" variant="bodySmall">
+        For additional information, please refer to the project{' '}
+        <Link href="https://git.uibk.ac.at/informatik/qe/swess23/group4/g4t3/-/wikis/home">
+          wiki
+        </Link>{' '}
+        and{' '}
+        <Link href="https://git.uibk.ac.at/informatik/qe/swess23/group4/g4t3/-/blob/690e66b84beca72c807af26bdeec6f2d37fd7929/README.md">
+          README
+        </Link>
+        .
+      </Typography>
 
+      <Stack spacing={2} sx={{ padding: theme.spacing(2, 0) }}>
         <Card sx={{ padding: 1.5 }}>
           <Typography variant="h6">Connecting an Access Point</Typography>
           <ol>
