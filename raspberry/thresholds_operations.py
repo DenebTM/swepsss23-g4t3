6978e2b39d2ecc31f6d17ae0b448a1081a32f587
csaw5791 <Andreas.Schett@student.uibk.ac.at>
2023-05-18 15:58:43 +0200

    Implement bleak logging

--- raspberry/thresholds_operations.py
+++ raspberry/thresholds_operations.py
@@ -1,7 +1,9 @@
 import common
+from bleak import BleakError
 from database_operations import get_sensorstation_thresholds, get_sensor_data_averages
 from rest_operations import send_warning_to_backend, clear_warning_on_backend
 import asyncio
+import logging_operations
 
 async def check_values_for_thresholds(sensorstation_client, sensorstation_id, session):
     try:
@@ -22,8 +24,8 @@ async def check_values_for_thresholds(sensorstation_client, sensorstation_id, se
                 await send_warning_to_sensorstation(sensorstation_client, sensorstation_id, sensor, session)
                 await send_warning_to_backend(sensorstation_id, session)
     except Exception as e:
-        print(e)
-        #TODO log error code
+        await logging_operations.log_to_file_and_list('Warning', f'Error in threshold check: {sensorstation_id}', entity_type='SENSOR_STATION', entity_id=str(sensorstation_id))
+        
                         
 async def send_warning_to_sensorstation(sensorstation_client, sensorstation_id, sensor, session):
     errorCode = 1
@@ -31,12 +33,13 @@ async def send_warning_to_sensorstation(sensorstation_client, sensorstation_id, 
     try:
         sensor_uuid = common.failure_uuids[sensor]
         await sensorstation_client.write_gatt_char(sensor_uuid, errorCodeByteArray)
+        await logging_operations.log_to_file_and_list('INFO', f'Activated error signal on sensorstation: {sensorstation_id}, for sensor {sensor}', entity_type='SENSOR_STATION', entity_id=str(sensorstation_id))
         await sensorstation_client.start_notify(
             common.warning_active_uuid,
             lambda char, data: asyncio.create_task(
                 clear_warning_on_backend(sensorstation_id, session, data)
             )
         )
-    except:
-        print('couldnt write to gatt')
-        #TODO log error code       
+    except BleakError as e:
+        await logging_operations.log_to_file_and_list('ERROR', f'Couldnt write to gatt characteristic for sensorstation {sensorstation_id}. Error: {e}', entity_type='SENSOR_STATION', entity_id=str(sensorstation_id))
+           
