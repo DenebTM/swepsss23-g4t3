699dfa5936486aee70dab1735b17c6a750514b99
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-05-19 11:31:00 +0200

    comment order

--- src/main/java/at/qe/skeleton/configs/WebSecurityConfig.java
+++ src/main/java/at/qe/skeleton/configs/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/api/access-points")
                 .permitAll()
 
-                // Allow visitors to upload photos and view the gallery
+                // Allow visitors to new the gallery and upload photos
                 .requestMatchers(HttpMethod.GET, "/api/sensor-stations/**")
                 .permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/sensor-stations/*/photos")
