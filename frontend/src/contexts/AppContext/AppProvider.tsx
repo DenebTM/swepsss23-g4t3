20cf5820da35086ac4aeb502fe3bc5d5a64c0073
Jamie Fox <james.fox@student.uibk.ac.at>
2023-05-19 11:16:04 +0200

    Collapse sidebar on narrower screens

--- frontend/src/contexts/AppContext/AppProvider.tsx
+++ frontend/src/contexts/AppContext/AppProvider.tsx
@@ -1,6 +1,15 @@
-import React, { Reducer, useCallback, useMemo, useReducer } from 'react'
+import React, {
+  Reducer,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+} from 'react'
+
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 import { SensorStation } from '~/models/sensorStation'
+import { theme } from '~/styles/theme'
 
 import { AppContext, initialAppState } from './AppContext'
 import { appReducer } from './appReducer'
@@ -19,10 +28,16 @@ interface AppProviderProps {
  * Wrapper component to allow accessing global values in child components without prop drilling.
  */
 const AppProvider: React.FC<AppProviderProps> = (props) => {
+  const breakMd = useMediaQuery(theme.breakpoints.down('md'))
   const [appState, dispatch] = useReducer<
     Reducer<AppState, AppReducerAction>,
     AppState
   >(appReducer, initialAppState, () => initialAppState)
+
+  /** Auto-hide the sidebar on narrower screens on page load */
+  useEffect(() => {
+    setSidebarOpen(!breakMd)
+  }, [])
 
   /** Dispatch action to set sensor stations in the reducer */
   const setSensorStations = useCallback(
