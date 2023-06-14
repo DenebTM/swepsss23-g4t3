39416f2d23bf024d69c9dc21501a338378f7ad37
csaw5791 <a.schett@student.uibk.ac.at>
2023-06-14 12:12:31 +0200

    remove http from basic address as it is now stored in common.py

--- raspberry/main.py
+++ raspberry/main.py
@@ -98,7 +98,7 @@ async def polling_loop(connection_request, session):
 async def main():
     while True:
         try:
-            async with aiohttp.ClientSession(base_url='http://'+common.web_server_address, raise_for_status=True) as session:
+            async with aiohttp.ClientSession(base_url=common.web_server_address, raise_for_status=True) as session:
                 connection_request = asyncio.Future()
                 await rest_operations.initialize_accesspoint(session)
 
