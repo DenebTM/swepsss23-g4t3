b7d60674c8730b41ec9cb0035609f1fe37742044
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-06-15 15:41:12 +0200

    Update startup script, add systemd service file

--- raspberry/startup_script.sh
+++ raspberry/startup_script.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-#move this to /etc/init.d/
-config_yaml_path="/config.yaml"
-python_path="/main.py"
-config_path="/.configure"
+sigint_handler() {
+    true
+}
+trap sigint_handler SIGINT
+
+clear
+
+basedir="/srv/planthealth"
+config_yaml_path="${basedir}/conf.yaml"
+python_path="${basedir}/main.py"
+config_path="${basedir}/configure"
 
 if [ -f "$config_yaml_path" ]; then
-    echo "File found. Running python script."
-    /usr/bin/python3 "$python_script"
-else
-    echo "config.yaml not found. Running configure file first"
-    /bin/bash "$config_path"
-    echo "config.yaml was created. Running python script now"
+    echo "conf.yaml exists, running Python script."
     /usr/bin/python3 "$python_path"
+else
+    ( echo "conf.yaml not found, running configure script first."
+    /bin/bash "$config_path" ) &&
+
+    ( echo "conf.yaml created, running python script now."
+    /usr/bin/python3 "$python_path" ) ||
+
+    ( echo "Configure script aborted, dropping to shell."
+    /usr/bin/sudo -u pi /bin/bash )
 fi
