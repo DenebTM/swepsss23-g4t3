a828789d05d32139b37f803310cc36cf0d890b0b
Jamie Fox <james.fox@student.uibk.ac.at>
2023-06-15 19:15:54 +0200

    Add test for Add Greenhouse modal

--- frontend/src/tests/admin/AdminHome.test.tsx
+++ frontend/src/tests/admin/AdminHome.test.tsx
@@ -1,7 +1,22 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { test } from 'vitest'
 import { AdminHome } from '~/components/admin/home/AdminHome'
 
 test('render AdminHome without crashing', async () => {
   render(<AdminHome />)
+})
+
+test('open Add Greenhouse dialog from AdminHome', async () => {
+  render(<AdminHome />)
+
+  // Find and click on button to add a new greenhouse
+  const addGreenhousesBtn = await screen.findByText('Add Greenhouses')
+  await fireEvent.click(addGreenhousesBtn)
+
+  // Check that the dialog is visible in the screen. qqjf TODO update text to constant.
+  expect(
+    screen.getByText(
+      'Pair with a new sensor station via BLE to start monitoring your plants'
+    )
+  ).toBeInTheDocument()
 })
