3df0dd447cc9c9b1de6cfce768383d2b8bfbfc11
Jamie Fox <james.fox@student.uibk.ac.at>
2023-06-15 19:59:55 +0200

    Add test for photo upload page body

--- frontend/src/tests/PhotoUpload/PhotoUpload.test.tsx
+++ frontend/src/tests/PhotoUpload/PhotoUpload.test.tsx
@@ -1,7 +1,38 @@
-import { render } from '@testing-library/react'
-import { test } from 'vitest'
+import { faker } from '@faker-js/faker'
+import { render, screen } from '@testing-library/react'
+import { test, vi } from 'vitest'
+import {
+  decryptSensorStationUuid,
+  encryptSensorStationUuid,
+  SS_UUID_PARAM,
+} from '~/common'
 import { PhotoUpload } from '~/components/photoUpload/PhotoUpload'
+import { SensorStationUuid } from '~/models/sensorStation'
+
+/** Mocked UUID for testing */
+const mockSsId: SensorStationUuid = faker.datatype.number({ min: 0, max: 15 })
+const encryptedUuid = encryptSensorStationUuid(mockSsId)
+
+/** Mock passing in the sensor station ID via search params */
+vi.mock('react-router-dom', () => ({
+  useSearchParams: (): [URLSearchParams] => [
+    new URLSearchParams([[SS_UUID_PARAM, decodeURIComponent(encryptedUuid)]]),
+  ],
+  useParams: () => vi.fn(),
+  useNavigate: () => vi.fn(),
+  useLocation: () => vi.fn(),
+  useRouteError: () => vi.fn(),
+}))
 
 test('renders photo upload page without crashing', () => {
   render(<PhotoUpload />)
+
+  // Assert that the page heading is displayed
+  expect(screen.getByText('Welcome to PlantHealth')).toBeInTheDocument()
+})
+
+test('decryptSensorStationUuid is the inverse of encryptSensorStationUuid', () => {
+  expect(decryptSensorStationUuid(decodeURIComponent(encryptedUuid))).toEqual(
+    mockSsId
+  )
 })
--- frontend/src/tests/admin/GenerateQrCode.test.tsx
+++ frontend/src/tests/admin/GenerateQrCode.test.tsx
@@ -1,22 +1,24 @@
+import { faker } from '@faker-js/faker'
 import { fireEvent, render, screen } from '@testing-library/react'
 import { test } from 'vitest'
 import { GenerateQrCode } from '~/components/admin/greenhouses/SensorStationsTable/GenerateQrCode/GenerateQrCode'
 import { SensorStationUuid } from '~/models/sensorStation'
 
-const MOCK_SSID: SensorStationUuid = 1
+/** Mocked sensor station UUID for testing */
+const mockSsId: SensorStationUuid = faker.datatype.number({ min: 0, max: 15 })
 
 test('render QR code modal without crashing', async () => {
-  render(<GenerateQrCode ssID={MOCK_SSID} />)
+  render(<GenerateQrCode ssID={mockSsId} />)
 })
 
 test('open QR code modal on button click', async () => {
-  render(<GenerateQrCode ssID={MOCK_SSID} />)
+  render(<GenerateQrCode ssID={mockSsId} />)
 
   const qrModalButton = await screen.findByRole('button')
   await fireEvent.click(qrModalButton)
 
   // Check that the dialog title is visible in the screen
-  expect(screen.getByText(`Greenhouse ${MOCK_SSID}`)).toBeInTheDocument()
+  expect(screen.getByText(`Greenhouse ${mockSsId}`)).toBeInTheDocument()
 
   // Check that the print button is visible
   expect(screen.getByText('Print')).toBeInTheDocument()
