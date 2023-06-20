17cebebe7d201a286925bbfab3799250069691cd
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-06-20 11:29:59 +0200

    fix tests

--- raspberry/tests/test_database_operations.py
+++ raspberry/tests/test_database_operations.py
@@ -10,12 +10,12 @@ AGGREGATION_PERIOD = 300
 MOCK_VALUES_TUPLE = (10,20,30,40,50,60)
 MOCK_THRESHOLDS_TUPLE = (100,100,100,100,100,100,0,0,0,0,0,0)
 
-class TestDatabaseOperations(unittest.IsolatedAsyncioTestCase):
+class TestDatabaseOperations(unittest.TestCase):
 
     @patch('database_operations.db_conn')
-    async def test_save_sensor_values_to_database(self, db_conn):
+    def test_save_sensor_values_to_database(self, db_conn):
         # Call the function with test input
-        await save_sensor_values_to_database(SENSORSTATION_ID, MOCK_VALUES_TUPLE[0], MOCK_VALUES_TUPLE[1], MOCK_VALUES_TUPLE[2], MOCK_VALUES_TUPLE[3], MOCK_VALUES_TUPLE[4], MOCK_VALUES_TUPLE[5])
+        save_sensor_values_to_database(SENSORSTATION_ID, MOCK_VALUES_TUPLE[0], MOCK_VALUES_TUPLE[1], MOCK_VALUES_TUPLE[2], MOCK_VALUES_TUPLE[3], MOCK_VALUES_TUPLE[4], MOCK_VALUES_TUPLE[5])
 
         # Assert that the mock database connection was called with the correct SQL query and parameters
         db_conn.execute.assert_called_once_with('INSERT INTO sensordata (ssID, temperature, humidity, air_pressure, illuminance, air_quality_index, soil_moisture, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
@@ -24,7 +24,7 @@ class TestDatabaseOperations(unittest.IsolatedAsyncioTestCase):
 
     # Test get_sensor_data_averages function
     @patch('database_operations.db_conn')
-    async def test_get_sensor_data_averages(self, db_conn):
+    def test_get_sensor_data_averages(self, db_conn):
         cursor = MagicMock()
         db_conn.cursor.return_value = cursor
         cursor.fetchone.return_value = MOCK_VALUES_TUPLE
@@ -39,25 +39,25 @@ class TestDatabaseOperations(unittest.IsolatedAsyncioTestCase):
             'soilMoisture': MOCK_VALUES_TUPLE[5]
         }
         # Call the function with the mocked parameters
-        result = await get_sensor_data_averages(SENSORSTATION_ID)
+        result = get_sensor_data_averages(SENSORSTATION_ID)
 
         # Check the results against the expected results
         self.assertEqual(result, expected_results)
 
     @patch('database_operations.db_conn')
-    async def test_get_sensorstation_aggregation_period(self, db_conn):
+    def test_get_sensorstation_aggregation_period(self, db_conn):
         cursor = MagicMock()
         db_conn.cursor.return_value = cursor
         cursor.fetchone.return_value = (AGGREGATION_PERIOD,)
        
         # Call the function with a mock sensorstation_id
-        transmission_interval = await get_sensorstation_aggregation_period(SENSORSTATION_ID)
+        transmission_interval = get_sensorstation_aggregation_period(SENSORSTATION_ID)
         # Check that the function returned the expected value
         self.assertEqual(transmission_interval, AGGREGATION_PERIOD)
 
     # Test get_sensor_data_thresholds function
     @patch('database_operations.db_conn')
-    async def test_get_sensor_data_thresholds(self, db_conn):
+    def test_get_sensor_data_thresholds(self, db_conn):
         # Mock the database connection and cursor and the query
         cursor = MagicMock()
         db_conn.cursor.return_value = cursor
@@ -80,13 +80,13 @@ class TestDatabaseOperations(unittest.IsolatedAsyncioTestCase):
         }
 
         # Call the function with the mocked parameters
-        result = await get_sensorstation_thresholds(SENSORSTATION_ID)
+        result = get_sensorstation_thresholds(SENSORSTATION_ID)
 
         # Check the results against the expected results
         self.assertEqual(result, expected_results)
 
     @patch('database_operations.db_conn')
-    async def test_update_sensorstation(self, db_conn):
+    def test_update_sensorstation(self, db_conn):
         #set up json which is received 
         json_data = {
             'ssID': SENSORSTATION_ID,
@@ -114,18 +114,19 @@ class TestDatabaseOperations(unittest.IsolatedAsyncioTestCase):
                 'temperature': MOCK_THRESHOLDS_TUPLE[0]
             }
         } 
-        await update_sensorstation(json_data)
+        update_sensorstation(json_data)
 
         # check if the data is inserted correctly
+        # (the indentation of the SQL string is important, don't change it)
         db_conn.execute.assert_called_once_with(
-                '''INSERT OR REPLACE INTO sensorstations
-                (ssID, aggregation_period,
-                temperature_max, humidity_max, airPressure_max, lightIntensity_max,
-                airQuality_max, soilMoisture_max,
-                temperature_min, humidity_min, airPressure_min, lightIntensity_min,
-                airQuality_min, soilMoisture_min)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                (SENSORSTATION_ID, AGGREGATION_PERIOD, MOCK_THRESHOLDS_TUPLE[0], MOCK_THRESHOLDS_TUPLE[1], 
-                 MOCK_THRESHOLDS_TUPLE[2], MOCK_THRESHOLDS_TUPLE[3], MOCK_THRESHOLDS_TUPLE[4], MOCK_THRESHOLDS_TUPLE[5], 
-                 MOCK_THRESHOLDS_TUPLE[6], MOCK_THRESHOLDS_TUPLE[7], MOCK_THRESHOLDS_TUPLE[8], MOCK_THRESHOLDS_TUPLE[9], 
-                 MOCK_THRESHOLDS_TUPLE[10], MOCK_THRESHOLDS_TUPLE[11]))
+        '''INSERT OR REPLACE INTO sensorstations
+        (ssID, aggregation_period,
+        temperature_max, humidity_max, airPressure_max, lightIntensity_max,
+        airQuality_max, soilMoisture_max,
+        temperature_min, humidity_min, airPressure_min, lightIntensity_min,
+        airQuality_min, soilMoisture_min)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+        (SENSORSTATION_ID, AGGREGATION_PERIOD, MOCK_THRESHOLDS_TUPLE[0], MOCK_THRESHOLDS_TUPLE[1], 
+        MOCK_THRESHOLDS_TUPLE[2], MOCK_THRESHOLDS_TUPLE[3], MOCK_THRESHOLDS_TUPLE[4], MOCK_THRESHOLDS_TUPLE[5], 
+        MOCK_THRESHOLDS_TUPLE[6], MOCK_THRESHOLDS_TUPLE[7], MOCK_THRESHOLDS_TUPLE[8], MOCK_THRESHOLDS_TUPLE[9], 
+        MOCK_THRESHOLDS_TUPLE[10], MOCK_THRESHOLDS_TUPLE[11]))
