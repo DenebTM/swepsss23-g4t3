7236fcd13b8faabfe21e1f2afda0bd1ec02a492c
Calvin Björn Feliz Hoy <calvin.hoy@student.uibk.ac.at>
2023-06-20 10:14:03 +0200

    Raspberry: Mostly use DEBUG log level instead of INFO

--- raspberry/thresholds_operations.py
+++ raspberry/thresholds_operations.py
@@ -10,8 +10,10 @@ async def check_values_for_thresholds(sensorstation_client, sensorstation_id, se
         thresholds_dict = await get_sensorstation_thresholds(sensorstation_id)
         averages_dict = await get_sensor_data_averages(sensorstation_id)
 
-        if thresholds_dict is None or averages_dict is None:
-            raise ValueError('Received None value for thresholds or averages.')
+        if thresholds_dict is None:
+            raise ValueError('Received None value for thresholds.')
+        if averages_dict is None:
+            raise ValueError('Received None value for averages.')
 
         for sensor, average_value in averages_dict.items():
             max_threshold = thresholds_dict.get(sensor+'_max')
@@ -36,13 +38,17 @@ async def send_warning_to_sensorstation(sensorstation_client, sensorstation_id, 
     try:
         sensor_uuid = common.failure_uuids[sensor]
         await sensorstation_client.write_gatt_char(sensor_uuid, errorCodeByteArray)
-        log_local_and_remote('INFO', f'Activated error signal on sensorstation: {sensorstation_id}, for sensor {sensor}', entity_type='SENSOR_STATION', entity_id=str(sensorstation_id))
+        log_local_and_remote('DEBUG', f'Set warning for {sensor} on station {sensorstation_id}', entity_type='SENSOR_STATION', entity_id=str(sensorstation_id))
+        
+        # clear warning if characteristic value was updated to 0
+        # TODO Refactor (i think this currently sets up another handler every single time a warning is set)
         await sensorstation_client.start_notify(
             common.warning_active_uuid,
-            lambda char, data: asyncio.create_task(
-                clear_warning_on_backend(sensorstation_id, session, data)
-            )
+            lambda _, data: 
+                asyncio.create_task(clear_warning_on_backend(sensorstation_id, session))
+                    if int.from_bytes(data, 'little', signed=False) == 0
+                    else False
         )
     except BleakError as e:
-        log_local_and_remote('ERROR', f'Could not write to gatt characteristic for sensorstation {sensorstation_id}. Error: {e}', entity_type='SENSOR_STATION', entity_id=str(sensorstation_id))
+        log_local_and_remote('ERROR', f'Failed to set warning for {sensor} on station {sensorstation_id}: {e}', entity_type='SENSOR_STATION', entity_id=str(sensorstation_id))
            
