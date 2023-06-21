696499f832f8ea01b9211262a4ab35b49710595c
REDACTED-1 <REDACTED-1@student.uibk.ac.at>
2023-06-21 19:38:40 +0200

    Allow admins to view greenhouses in My Greenhouses

--- frontend/src/components/greenhouses/myGreenhouses/MyGreenhouses.tsx
+++ frontend/src/components/greenhouses/myGreenhouses/MyGreenhouses.tsx
@@ -8,7 +8,7 @@ import { PageHeader } from '~/components/page/PageHeader'
 import { PageTitle } from '~/components/page/PageTitle'
 import { PageWrapper } from '~/components/page/PageWrapper'
 import { useLoadSensorStations, useSensorStations } from '~/hooks/appContext'
-import { useUsername } from '~/hooks/user'
+import { useIsAdmin, useUsername } from '~/hooks/user'
 import { SensorStation, SensorStationUuid } from '~/models/sensorStation'
 
 import { GreenhouseAccordion } from './GreenhouseAccordion/GreenhouseAccordion'
@@ -20,6 +20,7 @@ export const MyGreenhouses: React.FC = () => {
   const sensorStations = useSensorStations(true)
   const loadSensorStations = useLoadSensorStations()
   const username = useUsername()
+  const isAdmin = useIsAdmin()
 
   // Store the currently expanded ssID in the state if an accordion is expanded, otherwise `null`
   const [expanded, setExpanded] = useState<SensorStationUuid | null>(null)
@@ -34,7 +35,7 @@ export const MyGreenhouses: React.FC = () => {
   }, [loadSensorStations])
 
   const filterSensorStations = (ss: SensorStation[]) =>
-    ss.filter((s) => s.gardeners.includes(username))
+    ss.filter((s) => isAdmin || s.gardeners.includes(username))
 
   return (
     <PageWrapper permittedRoles={PAGE_URL.myGreenhouses.permittedRoles}>
